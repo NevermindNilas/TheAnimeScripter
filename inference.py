565cd264a71b3d6794959a30c37484639a54aa3d
NevermindNilas <128264457+NevermindNilas@users.noreply.github.com>
2023-11-18 00:02:02 +0200

    working prototype of rife

--- inference.py
+++ inference.py
@@ -2,42 +2,98 @@ import cv2
 import os
 from threading import Thread
 import torch
+from torch.nn import functional as F
 import time
+from collections import deque
+import numpy as np
+from src.rife.pytorch_msssim import ssim_matlab
+from tqdm import tqdm
+import _thread
+from queue import Queue, Empty
 
-def process_video(video_file, output_path, model, height, width):
-    video_stream = VideoDecodeStream(video_file, width, height)
+def process_video_rife(video_file, output_path, model, scale, device, half):
+    video_stream = VideoDecodeStream(video_file)
     video_stream.start()
-    
-    i = 0
     prev_frame = video_stream.read()
-    time.sleep(1)
+    write_buffer = Queue(maxsize=500)
+    _thread.start_new_thread(clear_write_buffer, (output_path, write_buffer))
+    lastframe = prev_frame
+    h,w,_= video_stream.get_shape()
+    tmp = max(128, int(128 / scale))
+    ph = ((h - 1) // tmp + 1) * tmp
+    pw = ((w - 1) // tmp + 1) * tmp
+    padding = (0, pw - w, 0, ph - h)
+    multi = scale
+    progessbar = tqdm(total=video_stream.get_frame_count(), unit='frames')
+    I1 = torch.from_numpy(np.transpose(lastframe, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
+    I1 = pad_image(I1, half, padding)
+    temp = None
     while True:
-        next_frame = video_stream.read()
-        if next_frame is None:
+        if temp is not None:
+                frame = temp
+                temp = None
+        else:
+            frame = video_stream.read()
+        if frame is None:
             break
+        I0 = I1
+        I1 = torch.from_numpy(np.transpose(frame, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
+        I1 = pad_image(I1, half, padding)
+        I0_small = F.interpolate(I0, (32, 32), mode='bilinear', align_corners=False)
+        I1_small = F.interpolate(I1, (32, 32), mode='bilinear', align_corners=False)
+        break_flag = False
+        
+        output = make_inference(I0, I1, scale-1, scale, model)
+        
+        write_buffer.put(lastframe)
+        for mid in output:
+            mid = (((mid[0] * 255.).byte().cpu().numpy().transpose(1, 2, 0)))
+            write_buffer.put(mid[:h, :w])
+                
+        progessbar.update(1)
+        lastframe = frame
+        if break_flag:
+            video_stream.stop()
+            break
+        
+def make_inference(I0, I1, n, scale, model):
+    if model.version >= 3.9:
+        res = []
+        for i in range(n):
+            res.append(model.inference(I0, I1, (i+1) * 1. / (n+1), scale))
+        return res
+    else:
+        middle = model.inference(I0, I1, scale)
+        if n == 1:
+            return [middle]
+        first_half = make_inference(I0, middle, n=n//2)
+        second_half = make_inference(middle, I1, n=n//2)
+        if n%2:
+            return [*first_half, middle, *second_half]
+        else:
+            return [*first_half, *second_half]
 
-        prev_frame_tensor = torch.from_numpy(prev_frame).unsqueeze(0).float().cuda()
-        next_frame_tensor = torch.from_numpy(next_frame).unsqueeze(0).float().cuda()
-        
-        with torch.no_grad():
-            # Pass the previous and next frames to the model
-            interpolated_frame_tensor = model.execute(prev_frame_tensor, next_frame_tensor, timestep=1.0)
+def pad_image(img, half, padding):
+    if(half):
+        return F.pad(img, padding).half()
+    else:
+        return F.pad(img, padding)
 
-        interpolated_frame = interpolated_frame_tensor
-        
-        cv2.imwrite(os.path.join(output_path, f'frame_{i}.png'), interpolated_frame)
-        
-        i += 1
-        prev_frame = next_frame
-    
+def clear_write_buffer(output_path, write_buffer):
+    cnt = 0
+    time.sleep(5)
+    while True:
+        item = write_buffer.get()
+        if item is None:
+            break
+        cv2.imwrite(os.path.join(output_path, f"{cnt:0>5d}.png"), item)
+        cnt += 1
+            
 class VideoDecodeStream:
-    def __init__(self, video_file, width, height):
+    def __init__(self, video_file):
         self.vcap = cv2.VideoCapture(video_file)
-        self.width = width
-        self.height = height
         self.decode_buffer = []
         self.grabbed , self.frame = self.vcap.read()
-        #self.frame = cv2.resize(self.frame, (self.width, self.height))
         self.decode_buffer.append(self.frame)
         self.thread = Thread(target=self.update, args=())
         self.thread.daemon = True 
@@ -50,16 +106,16 @@ class VideoDecodeStream:
         while not self.stopped:
             grabbed, frame = self.vcap.read()
             if not grabbed:
-                print('The video buffering has been finished')
                 self.stopped = True
                 break
-            #frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
             self.decode_buffer.append(frame)
         self.vcap.release()
 
     def read(self):
-        return self.decode_buffer.pop(0) if self.decode_buffer else None
-
+        try:
+            return self.decode_buffer.pop(0)
+        except:
+            return None
     def stop(self):
         self.stopped = True 
 
@@ -71,3 +127,7 @@ class VideoDecodeStream:
 
     def get_fourcc(self):
         return cv2.VideoWriter_fourcc(*'mp4v')
+    
+    def get_shape(self):
+        return self.frame.shape
+    
