9fd846c0d76385f2b3608d00261e86f961d4f698
NevermindNilas <128264457+NevermindNilas@users.noreply.github.com>
2023-11-26 02:02:14 +0200

    Rife update more accurate, added warnings

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from src.dedup.dedup import Dedup
 from src.swinir.swinir import Swin
 #from src.segment.segment import Segment
 from src.compact.compact import Compact
-import cv2   
+import cv2
+
+os.environ["CUDA_MODULE_LOADING"] = "LAZY"
 
 def generate_output_filename(input_dir, filename_without_ext, extension):
     return os.path.join(input_dir, f"{filename_without_ext}_{extension}.mp4")
@@ -29,15 +31,21 @@ def main(video_file, model_type, half, multi, kind_model, pro, nt):
     output = generate_output_filename(input_dir, filename_without_ext, model_type)
     model_type = model_type.lower()
     
+    if h > 1080:
+        print("If you are using CUDA, you might run into CUDA out of memory errors, if so, try to lower the multithread size, autotiling will be added in the future to avoid this issue")
+    
     if model_type == "rife":
 
         UHD = True if w >= 2160 or h >= 2160 else False
         # UHD mode is auto decided by the script in order to avoid user errors.
 
-        Rife(video_file, output, UHD, 1, multi, half, w, h, nt, fps, tot_frame)
+        Rife(video_file, output, UHD, 1, multi, half, w, h, nt, fps, tot_frame, kind_model)
         
     elif model_type == "cugan" or model_type == "shufflecugan":
         
+        if model_type == "cugan" and h > 1080:
+            print("This may take a while, preferably use shufflecugan for over 1080p videos")
+            
         if model_type == "shufflecugan" and multi != 2:
             print("The only scale that Shufflecugan works with is 2x, auto setting scale to 2")
             multi = 2
