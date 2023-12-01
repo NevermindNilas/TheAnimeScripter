cd3956aa66417b33c20894825d8621a2a5ab70fa
NevermindNilas <nilascontact@gmail.com>
2023-12-01 10:50:05 -0800

    Fix model download and update scripts

--- update.bat
+++ update.bat
@@ -1,6 +1,11 @@
 @echo off
 echo ------------------------------------
-echo DOWNLOADING DEPENDENCIES
+echo         DOWNLOADING MODELS
+echo ------------------------------------
+python .\download_models.py
+
+echo ------------------------------------
+echo      DOWNLOADING DEPENDENCIES
 echo ------------------------------------
 python -m pip install --upgrade pip
 pip install -r requirements.txt
