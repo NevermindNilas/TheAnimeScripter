4bcc1e3b3fee4e74b974fda3f3b284c08196e3f0
NevermindNilas <nilascontact@gmail.com>
2023-12-02 16:56:43 -0800

    Updated scripts

--- update.bat
+++ update.bat
@@ -1,8 +1,15 @@
 @echo off
-echo ------------------------------------
-echo         DOWNLOADING MODELS
-echo ------------------------------------
-python .\download_models.py
+set /p UserInput=Do you want to download the models? After Effects users will have to. (Y/N):
+if /I "%UserInput%" EQU "Y" (
+    echo ------------------------------------
+    echo         DOWNLOADING MODELS
+    echo ------------------------------------
+    python .\download_models.py
+) else (
+    echo The models will not be downloaded, models will be automatically downloaded on runtime or can be downloaded manually,
+    echo using 'python .\download_models.py' in the command line.
+    echo After Effects users will have to download them if they want to use the script.
+)
 
 echo ------------------------------------
 echo      DOWNLOADING DEPENDENCIES
