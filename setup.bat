c333d70ff4ddaede7fe8066fbe84aa122d5fa6eb
NevermindNilas <nilascontact@gmail.com>
2023-12-09 18:38:49 +0200

    Fixed some bugs

--- setup.bat
+++ setup.bat
@@ -23,12 +23,7 @@ if /I "%UserInput%" EQU "Y" (
 )
 
 echo ------------------------------------
-echo         DOWNLOADING MODELS
-echo ------------------------------------
-python .\download_models.py
-
-echo ------------------------------------
 echo      DOWNLOADING DEPENDENCIES
 echo ------------------------------------
-pip install -r requirements.txt
+call update.bat
 pause
