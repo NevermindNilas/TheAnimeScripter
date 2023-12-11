aa42eb748337f862674e86225286d23a6d3a0c2d
NevermindNilas <nilascontact@gmail.com>
2023-12-11 19:04:22 +0200

    Setup.bat should now be fully functional and fixed some edge cases bugs.

--- src/rife/RIFE_HDv3.py
+++ src/rife/RIFE_HDv3.py
@@ -1,12 +1,7 @@
 import torch
-import torch.nn as nn
-import numpy as np
 from torch.optim import AdamW
-import torch.optim as optim
-from .warplayer import warp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from .IFNet_HDv3 import *
-import torch.nn.functional as F
 from .loss import *
 import os
 
@@ -66,43 +61,9 @@ class Model:
                     False,
                 )
 
-    def save_model(self, path, rank=0):
-        if rank == 0:
-            torch.save(self.flownet.state_dict(), "{}/flownet.pkl".format(path))
-
     def inference(self, img0, img1, timestep=0.5, scale=1.0):
         imgs = torch.cat((img0, img1), 1)
         scale_list = [8 / scale, 4 / scale, 2 / scale, 1 / scale]
         flow, mask, merged = self.flownet(imgs, timestep, scale_list)
         return merged[3]
 
-    def update(self, imgs, gt, learning_rate=0, mul=1, training=True, flow_gt=None):
-        for param_group in self.optimG.param_groups:
-            param_group["lr"] = learning_rate
-        img0 = imgs[:, :3]
-        img1 = imgs[:, 3:]
-        if training:
-            self.train()
-        else:
-            self.eval()
-        scale = [8, 4, 2, 1]
-        flow, mask, merged = self.flownet(
-            torch.cat((imgs, gt), 1), scale=scale, training=training
-        )
-        loss_l1 = (merged[3] - gt).abs().mean()
-        loss_smooth = self.sobel(flow[3], flow[3] * 0).mean()
-        # loss_vgg = self.vgg(merged[2], gt)
-        if training:
-            self.optimG.zero_grad()
-            loss_G = loss_l1 + loss_cons + loss_smooth * 0.1
-            loss_G.backward()
-            self.optimG.step()
-        else:
-            flow_teacher = flow[2]
-        return merged[3], {
-            "mask": mask,
-            "flow": flow[3][:, :2],
-            "loss_l1": loss_l1,
-            "loss_cons": loss_cons,
-            "loss_smooth": loss_smooth,
-        }
