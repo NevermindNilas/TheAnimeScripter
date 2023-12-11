a45419f4205d3c83f90211616d3be9819f4f0cdb
NevermindNilas <nilascontact@gmail.com>
2023-12-11 02:34:24 +0200

    Better and faster handling of setup.bat now tested and fully functional, I hope

--- setup.bat
+++ setup.bat
@@ -1,17 +1,25 @@
 @echo off
-if "%~1"=="post-install" goto :post_install
 
-echo ------------------------------------
-echo    DOWNLOADING PYTHON INSTALLER
-echo ------------------------------------
-curl https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe --output %cd%\python-3.11.0-amd64.exe
+:: Honestly whoever came up with this is insane
+:: Venv approach coming in the future, it's just a pain overall
 
-echo ------------------------------------
-echo         INSTALLING PYTHON
-echo ------------------------------------
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    echo Running with admin perms. 
+) ELSE (
+    echo Run the script with admin perms.
+    pause
+    exit /b
+)
+
+cd /d %~dp0
+
+set PYTHON_URL=https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe
+
 set /p consent="Do you agree with installing Python 3.11 and adding it to System Path? This is necessary for the functionality of the script. (Y/N): "
 if /i "%consent%"=="Y" (
-    start /wait "" "%cd%\python-3.11.0-amd64.exe" /quiet InstallAllUsers=1 PrependPath=1
+    powershell -Command "& { iwr '%PYTHON_URL%' -OutFile python-installer.exe }"
+    python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 ) else (
     echo Python installation cancelled.
     echo Feel free to run the script again or refer to manual installation on the github page
@@ -19,33 +27,28 @@ if /i "%consent%"=="Y" (
     exit /b
 )
 
-echo ------------------------------------
-echo         RESTARTING SCRIPT
-echo ------------------------------------
-start cmd /k "%~dpnx0" post-install
-exit /b
+echo Installing requirements...
 
-:post_install
-echo ------------------------------------
-echo      DOWNLOADING DEPENDENCIES
-echo ------------------------------------
-call "%ProgramFiles%\Python311\Scripts\pip" install --no-warn-script-location -r requirements.txt
+:: Installing requirements
+start cmd /c "pip install -r requirements.txt && echo Requirements installation succeeded! || echo Requirements installation failed!"
 
-echo ------------------------------------
-echo         DOWNLOADING MODELS
-echo ------------------------------------
-call "%ProgramFiles%\Python311\python" download_models.py
+start cmd /c "python download_models.py"
 
-echo ------------------------------------
-echo CHECKING STATIC_FFMPEG INSTALLATION
-echo ------------------------------------
-static_ffmpeg -version >nul 2>&1
-if errorlevel 1 (
-    echo static_ffmpeg is not installed properly. Please run: static_ffmpeg in the terminal.
-    exit /b
-) else (
-    echo static_ffmpeg is installed properly.
-)
+echo handling ffmpeg...
 
-pause
-exit /b
+mkdir src\ffmpeg
+
+:: Handling ffmpeg
+set FFMPEG_URL=https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip
+powershell -Command "& { iwr '%FFMPEG_URL%' -OutFile src\ffmpeg\ffmpeg.zip }"
+
+powershell -Command "& { Expand-Archive -Path 'src\ffmpeg\ffmpeg.zip' -DestinationPath 'src\ffmpeg\tmp'; }"
+
+powershell -Command "& { Move-Item -Path 'src\ffmpeg\tmp\ffmpeg-*-win64-gpl\bin\ffmpeg.exe' -Destination 'src\ffmpeg'; }"
+
+powershell -Command "& { Remove-Item -Path 'src\ffmpeg\tmp' -Recurse -Force; }"
+
+powershell -Command "& { Remove-Item -Path 'src\ffmpeg\ffmpeg.zip'; }"
+
+:: Removing the python installer
+powershell -Command "& { Remove-Item -Path 'python-installer.exe'; }"
