69a00c39c21d2aa13c78ee9f3f294a9e9732e237
NevermindNilas <nilascontact@gmail.com>
2023-12-12 22:12:25 +0200

    Fixed path issues added ffmpeg bat

--- src/rife/RIFE_HDv3.py
+++ src/rife/RIFE_HDv3.py
@@ -1,13 +1,7 @@
 import torch
-import torch.nn as nn
-import numpy as np
 from torch.optim import AdamW
-import torch.optim as optim
-import itertools
-from .warplayer import warp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from .IFNet_HDv3 import *
-import torch.nn.functional as F
 from .loss import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,9 +17,6 @@ class Model:
         self.sobel = SOBEL()
         if local_rank != -1:
             self.flownet = DDP(self.flownet, device_ids=[local_rank], output_device=local_rank)
-
-    def train(self):
-        self.flownet.train()
 
     def eval(self):
         self.flownet.eval()
@@ -49,41 +40,9 @@ class Model:
             else:
                 self.flownet.load_state_dict(convert(torch.load('{}/flownet.pkl'.format(path), map_location ='cpu')), False)
         
-    def save_model(self, path, rank=0):
-        if rank == 0:
-            torch.save(self.flownet.state_dict(),'{}/flownet.pkl'.format(path))
-
     def inference(self, img0, img1, timestep=0.5, scale=1.0):
         imgs = torch.cat((img0, img1), 1)
         scale_list = [8/scale, 4/scale, 2/scale, 1/scale]
         flow, mask, merged = self.flownet(imgs, timestep, scale_list)
         return merged[3]
     
-    def update(self, imgs, gt, learning_rate=0, mul=1, training=True, flow_gt=None):
-        for param_group in self.optimG.param_groups:
-            param_group['lr'] = learning_rate
-        img0 = imgs[:, :3]
-        img1 = imgs[:, 3:]
-        if training:
-            self.train()
-        else:
-            self.eval()
-        scale = [8, 4, 2, 1]
-        flow, mask, merged = self.flownet(torch.cat((imgs, gt), 1), scale=scale, training=training)
-        loss_l1 = (merged[3] - gt).abs().mean()
-        loss_smooth = self.sobel(flow[3], flow[3]*0).mean()
-        # loss_vgg = self.vgg(merged[2], gt)
-        if training:
-            self.optimG.zero_grad()
-            loss_G = loss_l1 + loss_cons + loss_smooth * 0.1
-            loss_G.backward()
-            self.optimG.step()
-        else:
-            flow_teacher = flow[2]
-        return merged[3], {
-            'mask': mask,
-            'flow': flow[3][:, :2],
-            'loss_l1': loss_l1,
-            'loss_cons': loss_cons,
-            'loss_smooth': loss_smooth,
-            }
