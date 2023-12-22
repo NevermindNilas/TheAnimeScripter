edc8ab09fa4f32f978dbfcd4472968f1e3d18faf
Trenton <130304830+Trentonom0r3@users.noreply.github.com>
2023-12-22 08:32:09 -0600

    slight adjustments, will need more work to be fully functional (python wise)

--- CEP/dist/com.psc.TheAnimeScripter/entry.py
+++ CEP/dist/com.psc.TheAnimeScripter/entry.py
@@ -1,21 +1,22 @@
 import os
 from PyShiftCore import *
+from main import *
 
 app = App()
 
 def request_from_JS(upscale_model, denoise_option, dedup_opt,
                     interp_opt, upscale_opt, upscale_value, interp_value, output_folder):
     
-    # all options will be valid, except possibly output_folder, so verify that and create it if necessary
+    # Only arg we need to check is output folder, all others were already verified by JS.
+    
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     path = ""   
     
-    # dedupt opt, interp opt, upscale opt, all may be selected ,or only a few, but at least one must be selected
     if dedup_opt == False and interp_opt == False and upscale_opt == False:
         return "You must select at least one option."
-    # get the activeLayer
-    layer = app.project.activeLayer
+    
+    layer = app.project.activeLayer     # get the activeLayer
     
     if layer is None:
         return "No active layer selected."
@@ -28,40 +29,33 @@ def request_from_JS(upscale_model, denoise_option, dedup_opt,
     if path == "":
         return "No active footage selected."
     
-    #formate everything into a dictionary
-    request = {
-        "path": path,
-        "upscale_model": upscale_model,
-        "denoise_option": denoise_option,
-        "dedup_opt": dedup_opt,
-        "interp_opt": interp_opt,
-        "upscale_opt": upscale_opt,
-        "upscale_value": upscale_value,
-        "interp_value": interp_value,
-        "output_folder": output_folder
+    args = {
+        "input": path,
+        "output": output_folder,
+        "interpolate": interp_opt,
+        "interpolate_factor": interp_value,
+        "upscale": upscale_opt,
+        "upscale_factor": upscale_value,
+        "upscale_method": upscale_model,
+        "cugan_kind": denoise_option,
+        "dedup": dedup_opt,
+        "dedup_sens": 5,
+        "dedup_method": "ffmpeg",
+        "nt": 1,
+        "half": 1,
+        "inpoint": 0,
+        "outpoint": 0
     }
     
-    process_request(request)
-    app.reportInfo("Refresh the project, and you're set!")
+    process_request(args)
+    # to get out path, take the folder, add the name of the file
+    out_path = os.path.join(output_folder, os.path.basename(path))  # Or however things are named
+    source.replace("new_name", out_path) # Replace the source with the new one
     return "Success!"
 
 
 def process_request(data):
-    case = data["upscale_model"]
-    if case == "ShuffleCugan":
-        pass
-    elif case == "Compact":
-        pass
-    elif case == "UltraCompact":
-        pass
-    elif case == "SuperUltraCompact":
-        pass
-    elif case == "Cugan":
-        pass
-    elif case == "Cugan-amd":
-        pass
-    
-    
+    Main(data)
     
     
             
