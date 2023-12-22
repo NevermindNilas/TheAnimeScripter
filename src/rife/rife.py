6297018ef846b4f933ffd3024758f193f19751c8
NevermindNilas <nilascontact@gmail.com>
2023-12-22 20:08:09 +0200

    Cleaning and more logging

--- src/rife/rife.py
+++ src/rife/rife.py
@@ -6,26 +6,26 @@ from torch.nn import functional as F
 
 
 class Rife:
-    def __init__(self, interpolation_factor, half, frame_size, UHD):
+    def __init__(self, interpolation_factor, half, width, height, UHD):
         self.interpolation_factor = interpolation_factor
         self.half = half
-        self.frame_size = frame_size
         self.UHD = UHD
         self.scale = 1.0
-        self.width = self.frame_size[0]
-        self.height = self.frame_size[1]
+        self.width = width
+        self.height = height
         self.modelDir = os.path.dirname(os.path.realpath(__file__))
 
         self.handle_model()
 
     def handle_model(self):
-        if self.UHD == True and self.scale == 1.0:
+        if self.UHD == True:
             self.scale = 0.5
 
         tmp = max(128, int(128 / self.scale))
         ph = ((self.height - 1) // tmp + 1) * tmp
         pw = ((self.width - 1) // tmp + 1) * tmp
         self.padding = (0, pw - self.width, 0, ph - self.height)
+
 
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
@@ -42,9 +42,6 @@ class Rife:
                 "Cannot load RIFE model, please check your weights")
 
         self.model = Model()
-        if not hasattr(self.model, 'version'):
-            self.model.version = 0
-
         self.model.load_model(self.modelDir, -1)
         self.model.eval()
         self.model.device()
@@ -67,10 +64,10 @@ class Rife:
         buffer = []
         I0 = torch.from_numpy(np.transpose(I0, (2, 0, 1))).to(
             self.device, non_blocking=True).unsqueeze(0).float() / 255.
-        I0 = self.pad_image(I0)
-        
         I1 = torch.from_numpy(np.transpose(I1, (2, 0, 1))).to(
             self.device, non_blocking=True).unsqueeze(0).float() / 255.
+        
+        I0 = self.pad_image(I0)
         I1 = self.pad_image(I1)
         
         output = self.make_inference(I0, I1, n - 1)
