d1df1487c94140752457d71d3a00296ab3246583
NevermindNilas <nilascontact@gmail.com>
2023-12-23 02:43:09 +0200

    Added Swin-IR Anime based on Swin Transformer and utilzied Bubble's Model

--- src/swinir/swinir.py
+++ src/swinir/swinir.py
@@ -6,77 +6,86 @@ import numpy as np
 from .swinir_arch import SwinIR as SwinIR_model
 
 class Swinir():
-    def __init__(self, upscale_factor, half):
+    def __init__(self, upscale_factor, half, width, height):
         self.upscale_factor = upscale_factor
         self.half = half
+        self.width = width
+        self.height = height
+        self.weights_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "weights")
+        self.filename = "2x_Bubble_AnimeScale_SwinIR_Small_v1.pth"
+        self.model_path = os.path.join(self.weights_dir, self.filename)
+        self.url = "https://github.com/Bubblemint864/AI-Models/releases/download/2x_Bubble_AnimeScale_SwinIR_Small_v1/2x_Bubble_AnimeScale_SwinIR_Small_v1.pth"
 
         self.handle_models()
 
     def handle_models(self):
-        """
-        Only going to support 2x for now due to the memory requirements of the model
+        self.create_weights_dir()
+        self.download_model()
+        self.load_model()
 
-        There's a probability that it won't even work for most users without tiling, 
-        I will look into that later
-        """
+    def create_weights_dir(self):
+        if not os.path.exists(self.weights_dir):
+            os.makedirs(self.weights_dir)
 
-        model_hyperparams = {'upscale': 2, 'in_chans': 3, 'img_size': 64, 'window_size': 8,
-                             'img_range': 1., 'mlp_ratio': 2, 'resi_connection': '1conv'}
-
-        self.model = SwinIR_model(depths=[6] * 4, embed_dim=60, num_heads=[6] * 4,
-                                  upsampler='pixelshuffledirect', **model_hyperparams)
-
-        dir_name = os.path.dirname(os.path.abspath(__file__))
-        weights_dir = os.path.join(dir_name, "weights")
-
-        if not os.path.exists(weights_dir):
-            os.makedirs(weights_dir)
-
-        filename = "2x_Bubble_AnimeScale_SwinIR_Small_v1.pth"
-        if not os.path.exists(os.path.join(weights_dir, "2x_Bubble_AnimeScale_SwinIR_Small_v1.pth")):
+    def download_model(self):
+        if not os.path.exists(self.model_path):
             print(f"Downloading SWINIR model...")
-            url = "https://github.com/Bubblemint864/AI-Models/releases/download/2x_Bubble_AnimeScale_SwinIR_Small_v1/2x_Bubble_AnimeScale_SwinIR_Small_v1.pth"
-            response = requests.get(url)
+            response = requests.get(self.url)
             if response.status_code == 200:
-                with open(os.path.join(weights_dir, filename), "wb") as file:
+                with open(self.model_path, "wb") as file:
                     file.write(response.content)
-                    
-        model_path = os.path.join(weights_dir, filename)
-        
-        pretrained_weights = torch.load(os.path.join(model_path), map_location="cpu")
-        pretrained_weights = pretrained_weights['params']
-                
-        self.model.load_state_dict(pretrained_weights, strict=True)
+
+    def load_model(self):
+        pretrained_model = torch.load(self.model_path, map_location="cpu")
+        pretrained_model = pretrained_model["params"]
+
+        self.model = SwinIR_model(
+            upscale=2,
+            in_chans=3,
+            img_size=32,
+            window_size=8,
+            img_range=1.,
+            depths=[6, 6, 6, 6],
+            embed_dim=60,
+            num_heads=[6, 6, 6, 6],
+            mlp_ratio=2,
+            upsampler="pixelshuffle",
+            resi_connection="1conv",
+        )
+
+        self.model.load_state_dict(pretrained_model)
         self.model.eval().cuda() if torch.cuda.is_available() else self.model.eval()
-        
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        
+
         torch.set_grad_enabled(False)
         if torch.cuda.is_available():
             torch.backends.cudnn.enabled = True
             torch.backends.cudnn.benchmark = True
             if self.half:
                 torch.set_default_tensor_type(torch.cuda.HalfTensor)
-        
+                self.model.half()
+
     def inference(self, frame):
         if self.half:
             frame = frame.half()
         with torch.no_grad():
             return self.model(frame)
-        
+
     def pad_frame(self, frame):
-        frame = torch.cat([frame, torch.flip(frame, [2])], 2)[:, :, :self.h, :]
-        frame = torch.cat([frame, torch.flip(frame, [3])], 3)[:, :, :, :self.w]
-        return frame     
-    
-    @torch.inference_mode
-    def run(self, frame, frame_size):
+        frame = torch.cat([frame, torch.flip(frame, [2])], 2)[:, :, :self.height, :]
+        frame = torch.cat([frame, torch.flip(frame, [3])], 3)[:, :, :, :self.width]
+        return frame
+
+    def transform_frame(self, frame):
         frame = torch.from_numpy(frame.astype(np.float32)).permute(2, 0, 1).unsqueeze(0).div_(255)
-        
-        if frame_size[0] % 8 != 0 or frame_size[1] % 8 != 0:
-            frame = self.pad_frame(frame)           
-            
+        frame = frame.to(self.device)
+        if self.width % 8 != 0 or self.height % 8 != 0:
+            frame = self.pad_frame(frame)
+        return frame
+
+    @torch.inference_mode
+    def run(self, frame):
+        frame = self.transform_frame(frame)
         frame = self.inference(frame)
         frame = frame.squeeze(0).permute(1, 2, 0).mul_(255).clamp_(0, 255).byte()
         return frame.cpu().numpy()
-    
