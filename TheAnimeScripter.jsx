53f5460c2653547f47868e8f2e83ac637ecfc863
NevermindNilas <nilascontact@gmail.com>
2024-01-13 06:20:58 +0200

    added av1 to jsx

--- TheAnimeScripter.jsx
+++ TheAnimeScripter.jsx
@@ -522,7 +522,7 @@ var TheAnimeScripter = (function() {
     textEncoderSelection.preferredSize.width = 103;
     textEncoderSelection.helpTip = "Choose which encoder you want to utilize, in no specific order, NVENC for NVidia GPUs and QSV for Intel iGPUs";
 
-    var dropdownEncoder_array = ["X264", "-", "X264_Animation", "-", "NVENC_H264", "-", "NVENC_H265", "-", "QSV_H264", "-", "QSV_H265"];
+    var dropdownEncoder_array = ["X264", "-", "X264_Animation", "-" , "AV1", "-", "NVENC_H264", "-", "NVENC_H265", "-", "QSV_H264", "-", "QSV_H265", "-", "NVENC_AV1"];
     var dropdownEncoder = group8.add("dropdownlist", undefined, undefined, {
         name: "dropdownEncoder",
         items: dropdownEncoder_array
