8e379622d37f7ab83eec98498d5cec83d8b1d616
NevermindNilas <nilascontact@gmail.com>
2024-01-18 00:39:19 +0200

    updated README.md

--- src/rife/rife414/IFNet_HDv3.py
+++ src/rife/rife414/IFNet_HDv3.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from src.rife.warplayer import warp
-# from train_log.refine import *
 
 
 def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
