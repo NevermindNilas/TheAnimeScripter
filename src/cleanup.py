7af1a45e4a54572a4efcbb1fd73104adb40c4ada
NevermindNilas <nilascontact@gmail.com>
2024-01-19 06:01:06 +0200

    Added forced yuv420 encoding and bumped up the version number

--- src/cleanup.py
+++ src/cleanup.py
@@ -9,4 +9,21 @@ def delete_pycache(directory):
                 print(f'Deleting {pycache_folder}')
                 shutil.rmtree(pycache_folder)
 
-delete_pycache('./')
+def delete_weights(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.pth') or file.endswith('.pkl') or file.endswith('.cpkl'):
+                weight_file = os.path.join(root, file)
+                print(f'Deleting {weight_file}')
+                os.remove(weight_file)
+
+def delete_ffmpeg(directory):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    if os.path.exists(os.path.join(dir_path, 'ffmpeg')):
+        print(f'Deleting {os.path.join(dir_path, "ffmpeg")}')
+        shutil.rmtree(os.path.join(dir_path, 'ffmpeg'))    
+
+if __name__ == "__main__":
+    delete_pycache('./')
+    delete_weights('./')
+    delete_ffmpeg('./')
