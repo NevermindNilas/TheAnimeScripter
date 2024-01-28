25ca1adeb404057d5c6d4a38b06c0ed88e797fd5
NevermindNilas <nilascontact@gmail.com>
2024-01-28 10:53:14 +0200

    simplified ytdlp settings

--- src/ytdlp.py
+++ src/ytdlp.py
@@ -45,44 +45,9 @@ class ytdlp():
             ydl.download([self.link])
 
         if self.ytdlp_quality:
-            command = [ffmpeg_path, '-i', temp_name]
-            match self.encode_method:
-                # I know that superfast isn't exactly the best preset for x264, but I fear that someone will try to convert a 4k 24 min video
-                # On a i7 4770k and it will take 3 business days to finish
-                case "x264":
-                    command.extend(
-                        ['-c:v', 'libx264', '-preset', 'superfast', '-crf', '14'])
-                case "x264_animation":
-                    command.extend(
-                        ['-c:v', 'libx264', '-preset', 'superfast', '-tune', 'animation', '-crf', '14'])
-                case "nvenc_h264":
-                    command.extend(
-                        ['-c:v', 'h264_nvenc', '-preset', 'p1', '-cq', '14'])
-                case "nvenc_h265":
-                    command.extend(
-                        ['-c:v', 'hevc_nvenc', '-preset', 'p1', '-cq', '14'])
-                case "qsv_h264":
-                    command.extend(
-                        ['-c:v', 'h264_qsv', '-preset', 'veryfast', '-global_quality', '14'])
-                case "qsv_h265":
-                    command.extend(
-                        ['-c:v', 'hevc_qsv', '-preset', 'veryfast', '-global_quality', '14'])
-                case "nvenc_av1":
-                    command.extend(
-                        ['-c:v', 'av1_nvenc', '-preset', 'p1', '-cq', '14'])
-                case "av1":
-                    command.extend(
-                        ['-c:v', 'libsvtav1', '-preset', '8', '-crf', '14'])
-                case "h264_amf":
-                    command.extend(
-                        ['-c:v', 'h264_amf', '-quality', 'speed', '-rc', 'cqp', '-qp', '14'])
-                case "hevc_amf":
-                    command.extend(
-                        ['-c:v', 'hevc_amf', '-quality', 'speed', '-rc', 'cqp', '-qp', '14'])
-
-            command.append(self.output)
+            from .ffmpegSettings import encodeYTDLP
+            command = encodeYTDLP(temp_name, self.output, ffmpeg_path, self.encode_method)
             subprocess.run(command)
-
             os.remove(temp_name)
 
             logging.info(
