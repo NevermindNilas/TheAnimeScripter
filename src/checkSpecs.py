d133e0b1c0e30a341ef167450e46a7f3acd4b2d8
NevermindNilas <nilascontact@gmail.com>
2024-02-03 20:01:42 +0200

    Improved logging

--- src/checkSpecs.py
+++ src/checkSpecs.py
@@ -1,24 +1,21 @@
 import logging
-import GPUtil
-import psutil
+import wmi
 
 def checkSystem():
     try:
-        # Log GPU info
-        gpus = GPUtil.getGPUs()
-        if gpus:
-            for gpu in gpus:
-                logging.info(f"GPU: {gpu.name}")
-        else:
-            logging.info("No GPU detected")
+        logging.info("\n============== System Checker ==============")
+        computer = wmi.WMI()
+        os_info = computer.Win32_OperatingSystem()[0]
+        proc_info = computer.Win32_Processor()[0]
+        gpu_info = computer.Win32_VideoController()
+        os_name = os_info.Name.encode('utf-8').split(b'|')[0]
+        system_ram = float(os_info.TotalVisibleMemorySize) / 1048576  # KB to GB
 
-        # Log CPU info
-        # Will come back to this later
-
-        # Log RAM info
-        ram = psutil.virtual_memory()
-        logging.info(f"Total RAM: {ram.total / 1024**3:.2f} GB")
-        logging.info(f"Available RAM: {ram.available / 1024**3:.2f} GB")
+        logging.info(f"OS Name: {os_name}")
+        logging.info(f"CPU: {proc_info.Name}")
+        logging.info(f"RAM: {system_ram} GB")
+        for gpu in gpu_info:
+            logging.info(f"Graphics Card: {gpu.Name}")
 
     except Exception as e:
         logging.error(f"An error occurred while checking the system: {e}")
