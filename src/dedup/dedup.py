558d899da68fc4b686d1cdf997e8a6cbeb9aaabb
NevermindNilas <nilascontact@gmail.com>
2024-02-06 16:57:25 +0200

    added better logging and fixed a bug

--- src/dedup/dedup.py
+++ src/dedup/dedup.py
@@ -2,26 +2,35 @@ import subprocess
 import logging
 
 
-def dedup_ffmpeg(input, output, mpdecimate_params, ffmpeg_path, encode_method="libx264"):
+def dedup_ffmpeg(input, output, filters, ffmpeg_path, encode_method="libx264"):
     encode_options = handle_encoder(encode_method)
-    ffmpeg_command = [ffmpeg_path, "-i", input, "-vf", mpdecimate_params,
+    ffmpeg_command = [ffmpeg_path, "-hide_banner", "-i", input, "-vf", filters,
                       "-an"] + flatten_dict(encode_options) + [output]
 
     logging.info(f"Encoding options: {' '.join(ffmpeg_command)}")
+    
+    logging.info(
+        "\n============== FFMPEG Output Log ============")
+    
+    process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    subprocess.Popen(ffmpeg_command, stdout=subprocess.DEVNULL,
-                     stderr=subprocess.DEVNULL)
+    for line in iter(process.stdout.readline, b''):
+        logging.info(line.decode().strip())
 
-
-def trim_input_dedup(input, output, inpoint, outpoint, mpdecimate_params, ffmpeg_path, encode_method="libx264"):
+def trim_input_dedup(input, output, inpoint, outpoint, filters, ffmpeg_path, encode_method="libx264"):
     encode_options = handle_encoder(encode_method)
-    command = [ffmpeg_path, "-ss", str(inpoint), "-to", str(outpoint), "-i", input, "-vf", mpdecimate_params,
+    ffmpeg_command = [ffmpeg_path, "-hide_banner", "-ss", str(inpoint), "-to", str(outpoint), "-i", input, "-vf", filters,
                "-an"] + flatten_dict(encode_options) + [output, "-v", "quiet", "-stats"]
 
-    logging.info(f"Encoding options: {' '.join(command)}")
+    logging.info(f"Encoding options: {' '.join(ffmpeg_command)}")
+    
+    logging.info(
+        "\n============== FFMPEG Output Log ============")
+    
+    process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    subprocess.Popen(command, stdout=subprocess.DEVNULL,
-                     stderr=subprocess.DEVNULL)
+    for line in iter(process.stdout.readline, b''):
+        logging.info(line.decode().strip())
 
 
 def handle_encoder(encode_method):
