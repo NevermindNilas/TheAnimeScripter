c50861fbe50f6495febf78c554ceb6e867cc94dd
NevermindNilas <nilascontact@gmail.com>
2024-02-11 08:56:09 +0200

    modified gmfss

--- src/gmfss/gmfss_fortuna_union.py
+++ src/gmfss/gmfss_fortuna_union.py
@@ -3,10 +3,13 @@ import wget
 import torch
 import numpy as np
 import logging
+import zipfile
 
 from torch.nn import functional as F
 
 # from: https://github.com/HolyWu/vs-gmfss_fortuna/blob/master/vsgmfss_fortuna/__init__.py
+
+
 class GMFSS():
     def __init__(self, interpolation_factor, half, width, height, UHD, ensemble=False):
 
@@ -31,49 +34,57 @@ class GMFSS():
         self.handle_model()
 
     def handle_model(self):
-        # Apparently this can improve performance slightly
         torch.set_float32_matmul_precision("medium")
-        
+
         dir_path = os.path.dirname(os.path.realpath(__file__))
         weights_dir = os.path.join(dir_path, "weights")
-
+        
         if not os.path.exists(weights_dir):
-            os.mkdir(weights_dir)
+            os.makedirs(weights_dir)
 
-        url_list = ["https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/feat_base.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/feat_union.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/flownet.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/fusionnet_base.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/fusionnet_union.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/metric_base.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/metric_union.pkl",
-                    "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/rife.pkl",
-                    ]
+        files_to_check = [
+            "feat_base.pkl",
+            "feat_union.pkl",
+            "flownet.pkl",
+            "fusionnet_base.pkl",
+            "fusionnet_union.pkl",
+            "metric_base.pkl",
+            "metric_union.pkl",
+            "rife.pkl",
+        ]
 
-        download = any(not os.path.exists(os.path.join(weights_dir, url.split('/')[-1])) for url in url_list)
+        all_files_exist = all(os.path.exists(os.path.join(
+            weights_dir, filename)) for filename in files_to_check)
 
-        if download:
-            print("Downloading GMFSS models...")
-            for url in url_list:
-                filename = url.split('/')[-1]
-                if not os.path.exists(os.path.join(weights_dir, filename)):
-                    print(f"\nDownloading {filename}")
-                    wget.download(url, out=weights_dir)
-                else:
-                    print(f"\n{filename} already exists. Skipping download.")
-            print("\n")
-            
-        model_dir = os.path.join(dir_path, "weights")
+        if not all_files_exist:
+            zip_url = "https://github.com/NevermindNilas/TAS-Modes-Host/releases/download/main/gmfss-fortuna-union.zip"
+            zip_filename = zip_url.split('/')[-1]
+            zip_filepath = os.path.join(weights_dir, zip_filename)
+
+            print(f"\nDownloading {zip_filename}")
+            logging.info(
+                f"Downloading {zip_filename}")
+            wget.download(zip_url, out=weights_dir)
+
+            # Extract the zip file  
+            with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+                print(f"\nExtracting {zip_filename}")
+                zip_ref.extractall(weights_dir)
+                
+            os.remove(zip_filepath)
+        else:
+            logging.info("Weights already exist, skipping download")
+
         model_type = "union"
-        
+
         self.cuda_available = torch.cuda.is_available()
-        
+
         if not self.cuda_available:
             logging.info(
                 "CUDA is not available, using CPU. Expect significant slowdows or no functionality at all. If you have a NVIDIA GPU, please install CUDA and make sure that CUDA_Path is in the environment variables.")
             logging.info(
                 "CUDA Installation link: https://developer.nvidia.com/cuda-downloads")
-            
+
         self.device = torch.device("cuda" if self.cuda_available else "cpu")
 
         torch.set_grad_enabled(False)
@@ -85,21 +96,22 @@ class GMFSS():
 
         from .model.GMFSS import GMFSS as Model
 
-        self.model = Model(model_dir, model_type, self.scale, ensemble=self.ensemble)
+        self.model = Model(weights_dir, model_type,
+                           self.scale, ensemble=self.ensemble)
         self.model.eval().to(self.device, memory_format=torch.channels_last)
 
         self.dtype = torch.float
         if self.cuda_available and self.half:
             self.model.half()
             self.dtype = torch.half
-                
-              
+
     @torch.inference_mode()
     def make_inference(self, n):
-        timestep = torch.tensor((n+1) * 1. / (self.interpolation_factor+1), dtype=self.dtype, device=self.device)
+        timestep = torch.tensor(
+            (n+1) * 1. / (self.interpolation_factor+1), dtype=self.dtype, device=self.device)
         output = self.model(self.I0, self.I1, timestep)
         output = (((output[0] * 255.).byte().cpu().numpy().transpose(1, 2, 0)))
-        
+
         return output[:self.height, :self.width, :]
 
     @torch.inference_mode()
