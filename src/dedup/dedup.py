6b2a6ccc353ee6265d2f2d5788e0ba25268c31b7
NevermindNilas <nilascontact@gmail.com>
2024-02-17 02:09:23 +0200

    fixed dedup

--- src/dedup/dedup.py
+++ src/dedup/dedup.py
@@ -4,44 +4,35 @@ import logging
 from src.ffmpegSettings import match_encoder
 
 
-def dedup_ffmpeg(input, output, filters, ffmpeg_path, encode_method="libx264"):
+def dedupFFMPEG(
+    input,
+    output,
+    filters,
+    ffmpeg_path,
+    encode_method="libx264",
+    inpoint=None,
+    outpoint=None,
+):
     encode_options = match_encoder(encode_method)
 
     filters = " ".join(filters)
 
-    ffmpeg_command = [ffmpeg_path, "-hide_banner", "-i", input, "-vf", filters,
-                      "-an"] + encode_options + [output]
+    ffmpeg_command = [ffmpeg_path, "-hide_banner"]
+
+    if outpoint != 0:
+        ffmpeg_command += ["-ss", str(inpoint), "-to", str(outpoint)]
+
+    ffmpeg_command += (
+        ["-i", input, "-vf", filters, "-an"] + encode_options + [output, "-y"]
+    )
 
     logging.info(f"Encoding options: {' '.join(ffmpeg_command)}")
 
-    logging.info(
-        "\n============== FFMPEG Output Log ============")
-
-    ffmpeg_command = " ".join(ffmpeg_command)
-
-    print(ffmpeg_command)
-    process = subprocess.Popen(
-        ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    for line in iter(process.stdout.readline, b''):
-        logging.info(line.decode().strip())
-
-
-def trim_input_dedup(input, output, inpoint, outpoint, filters, ffmpeg_path, encode_method="libx264"):
-    encode_options = match_encoder(encode_method)
-
-    filters = " ".join(filters)
-
-    ffmpeg_command = [ffmpeg_path, "-hide_banner", "-ss", str(inpoint), "-to", str(outpoint), "-i", input, "-vf", filters,
-                      "-an"] + encode_options + [output, "-v", "quiet", "-stats"]
-
-    logging.info(f"Encoding options: {' '.join(ffmpeg_command)}")
-
-    logging.info(
-        "\n============== FFMPEG Output Log ============")
+    logging.info("\n============== FFMPEG Output Log ============")
 
     process = subprocess.Popen(
-        ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
 
-    for line in iter(process.stdout.readline, b''):
+    for line in iter(process.stdout.readline, b""):
         logging.info(line.decode().strip())
