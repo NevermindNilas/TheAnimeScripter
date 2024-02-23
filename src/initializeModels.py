bed1d3ed6f831a26ec6098a0547af59f32c91979
NevermindNilas <nilascontact@gmail.com>
2024-02-23 17:48:23 +0200

    added new param to keep original audio

--- src/initializeModels.py
+++ src/initializeModels.py
@@ -99,7 +99,7 @@ def intitialize_models(self):
                     self.height,
                     self.custom_model,
                 )
-
+                
     if self.interpolate:
         logging.info(
             f"Interpolating from {format(self.fps, '.3f')}fps to {format(self.fps * self.interpolate_factor, '.3f')}fps")
