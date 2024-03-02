56888db611741cfdf93d2fa4d6bd5b08d13c51b8
NevermindNilas <nilascontact@gmail.com>
2024-03-02 13:28:51 +0200

    fixed build.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if getattr(sys, "frozen", False):
 else:
     mainPath = os.path.dirname(os.path.abspath(__file__))
 
-scriptVersion = "1.3.3"
+scriptVersion = "1.3.4"
 warnings.filterwarnings("ignore")
 
 """
