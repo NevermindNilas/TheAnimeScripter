0f3b90eef94fdb7a4bb57e77fa37acf9dfbf4b5c
NevermindNilas <nilascontact@gmail.com>
2024-03-23 04:09:10 +0200

    was the clamping necessary?

--- src/unifiedUpscale.py
+++ src/unifiedUpscale.py
@@ -2,18 +2,13 @@ import os
 import torch
 import numpy as np
 import logging
-import tensorrt as trt
-import pycuda.driver as cuda
-import pycuda.autoinit
 
 from spandrel import ImageModelDescriptor, ModelLoader
 from .downloadModels import downloadModels, weightsDir, modelsMap
 
-
 # Apparently this can improve performance slightly
 torch.set_float32_matmul_precision("medium")
 
-TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE)
 
 
 class Upscaler:
@@ -57,6 +52,136 @@ class Upscaler:
         """
         Load the desired model
         """
+        self.trt = False
+        if not self.trt:
+            if not self.customModel:
+                self.filename = modelsMap(
+                    self.upscaleMethod, self.upscaleFactor, self.cuganKind
+                )
+                if not os.path.exists(
+                    os.path.join(weightsDir, self.upscaleMethod, self.filename)
+                ):
+                    modelPath = downloadModels(
+                        model=self.upscaleMethod,
+                        cuganKind=self.cuganKind,
+                        upscaleFactor=self.upscaleFactor,
+                    )
+
+                else:
+                    modelPath = os.path.join(
+                        weightsDir, self.upscaleMethod, self.filename
+                    )
+
+            else:
+                if os.path.isfile(self.customModel):
+                    modelPath = self.customModel
+
+                else:
+                    raise FileNotFoundError(
+                        f"Custom model file {self.customModel} not found"
+                    )
+
+            try:
+                self.model = ModelLoader().load_from_file(modelPath)
+            except Exception as e:
+                logging.error(f"Error loading model: {e}")
+
+            if self.customModel:
+                assert isinstance(self.model, ImageModelDescriptor)
+
+            self.isCudaAvailable = torch.cuda.is_available()
+            self.model = (
+                self.model.eval().cuda() if self.isCudaAvailable else self.model.eval()
+            )
+
+            self.device = torch.device("cuda" if self.isCudaAvailable else "cpu")
+
+            if self.isCudaAvailable:
+                #self.stream = [torch.cuda.Stream() for _ in range(self.nt)]
+                #self.currentStream = 0
+                torch.backends.cudnn.enabled = True
+                torch.backends.cudnn.benchmark = True
+                if self.half:
+                    torch.set_default_dtype(torch.float16)
+                    self.model.half()
+
+    @torch.inference_mode()
+    def run(self, frame: np.ndarray) -> np.ndarray:
+        """
+        Upscale a frame using a desired model, and return the upscaled frame
+        Expects a numpy array of shape (height, width, 3) and dtype uint8
+        """
+        with torch.no_grad():
+            frame = (
+                torch.from_numpy(frame)
+                .permute(2, 0, 1)
+                .unsqueeze(0)
+                .float()
+                .mul_(1 / 255)
+            )
+
+            frame = frame.contiguous(memory_format=torch.channels_last)
+
+            if self.isCudaAvailable:
+                #torch.cuda.set_stream(self.stream[self.currentStream])
+                frame = frame.cuda(non_blocking=True)
+                if self.half:
+                    frame = frame.half()
+
+            frame = self.model(frame)
+            frame = frame.squeeze(0).permute(1, 2, 0).mul_(255).byte()
+
+            """
+            if self.isCudaAvailable:
+                torch.cuda.synchronize(self.stream[self.currentStream])
+                self.currentStream = (self.currentStream + 1) % len(self.stream)
+            """
+
+            return frame.cpu().numpy()
+
+
+"""
+import os
+import torch
+import numpy as np
+import logging
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+
+from spandrel import ImageModelDescriptor, ModelLoader
+from .downloadModels import downloadModels, weightsDir, modelsMap
+
+# Apparently this can improve performance slightly
+torch.set_float32_matmul_precision("medium")
+
+TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE)
+
+
+class Upscaler:
+    def __init__(
+        self,
+        upscaleMethod: str = "shufflecugan",
+        upscaleFactor: int = 2,
+        cuganKind: str = "conservative",
+        half: bool = False,
+        width: int = 1920,
+        height: int = 1080,
+        customModel: str = None,
+        nt: int = 1,
+    ):
+        self.upscaleMethod = upscaleMethod
+        self.upscaleFactor = upscaleFactor
+        self.cuganKind = cuganKind
+        self.half = half
+        self.width = width
+        self.height = height
+        self.customModel = customModel
+        self.nt = nt
+
+        self.handleModel()
+
+    def handleModel(self):
         self.trt = False
         if not self.trt:
             if not self.customModel:
@@ -203,10 +328,7 @@ class Upscaler:
 
     @torch.inference_mode()
     def run(self, frame: np.ndarray) -> np.ndarray:
-        """
-        Upscale a frame using a desired model, and return the upscaled frame
-        Expects a numpy array of shape (height, width, 3) and dtype uint8
-        """
+    
         if not self.trt:
             with torch.no_grad():
                 frame = (
@@ -241,16 +363,8 @@ class Upscaler:
 
             frame = frame.reshape(1, 3, 1080, 1920)
             np.copyto(self.h_input, frame)
-
-            """
-            WTF IS
-            [03/20/2024-02:11:48] [TRT] [E] 1: [genericReformat.cuh::genericReformat::copyVectorizedRunKernel::1587] Error Code 1: Cuda Runtime (invalid resource handle)
-
-            Idk where have I messed up but I'm not able to figure it out
-            Piece of...
-            """
-            print(f"Shape of h_input: {self.h_input.shape}")
-            print(f"Shape of frame: {frame.shape}")
+            #print(f"Shape of h_input: {self.h_input.shape}")
+            #print(f"Shape of frame: {frame.shape}")
 
             cuda.memcpy_htod_async(self.d_input, self.h_input, self.stream)
             self.context.execute_async_v2(
@@ -268,3 +382,4 @@ class Upscaler:
             frame = frame.astype(np.uint8)
 
             return frame
+"""
