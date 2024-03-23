b207355e09978951879df2c132cfc2b22c8c1aca
NevermindNilas <nilascontact@gmail.com>
2024-03-23 05:48:57 +0200

    process youtube videos with the script and added -pix_fmt support for --custom_encoder

--- src/generateOutput.py
+++ src/generateOutput.py
@@ -1,20 +1,27 @@
 import os
 import random
 
+
 def outputNameGenerator(args):
     arg_map = {
-        'resize': f"-Re{args.resize_factor}",
-        'dedup': "-De",
-        'interpolate': f"-Int{args.interpolate_factor}",
-        'upscale': f"-Up{args.upscale_factor}",
-        'sharpen': f"-Sh{args.sharpen_sens}",
-        'denoise': f"-De{args.denoise_method}",
-        'segment': "-Segment",
-        'depth': "-Depth",
-        'ytdlp': "-YTDLP"
+        "resize": f"-Re{args.resize_factor}",
+        "dedup": "-De",
+        "interpolate": f"-Int{args.interpolate_factor}",
+        "upscale": f"-Up{args.upscale_factor}",
+        "sharpen": f"-Sh{args.sharpen_sens}",
+        "denoise": f"-De{args.denoise_method}",
+        "segment": "-Segment",
+        "depth": "-Depth",
+        "ytdlp": "-YTDLP",
     }
 
-    parts = [os.path.splitext(os.path.basename(args.input))[0] if args.input else "TAS"]
+    if "https://" in args.input or "http://" in args.input:
+        name = "TAS" + "-YTDLP" + f"-{random.randint(0, 1000)}" + ".mp4"
+        return name
+    else:
+        parts = [
+            os.path.splitext(os.path.basename(args.input))[0] if args.input else "TAS"
+        ]
 
     for arg, format_str in arg_map.items():
         if getattr(args, arg, None):
@@ -30,7 +37,7 @@ def outputNameGenerator(args):
         extension = os.path.splitext(args.input)[1]
     else:
         extension = ".mp4"
-        
+
     outputName = "".join(parts) + extension
-    
+
     return outputName
