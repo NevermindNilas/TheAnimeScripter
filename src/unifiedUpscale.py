d19f25a7c01f414bc7a25b7d88e74337d24fc50a
NevermindNilas <nilascontact@gmail.com>
2024-03-30 18:02:42 +0200

    working on directml

--- src/unifiedUpscale.py
+++ src/unifiedUpscale.py
@@ -2,6 +2,9 @@ import os
 import torch
 import numpy as np
 import logging
+import onnxruntime as ort
+import torch.nn.functional as F
+import cv2
 
 from spandrel import ImageModelDescriptor, ModelLoader
 from .downloadModels import downloadModels, weightsDir, modelsMap
@@ -84,7 +87,7 @@ class Upscaler:
         self.model = (
             self.model.eval().cuda() if self.isCudaAvailable else self.model.eval()
         )
-        
+
         self.device = torch.device("cuda" if self.isCudaAvailable else "cpu")
         if self.isCudaAvailable:
             # self.stream = [torch.cuda.Stream() for _ in range(self.nt)]
@@ -94,6 +97,16 @@ class Upscaler:
             if self.half:
                 torch.set_default_dtype(torch.float16)
                 self.model.half()
+
+    
+        self.padWidth = 0 if self.width % 8 == 0 else 8 - (self.width % 8)
+        self.padHeight = 0 if self.height % 8 == 0 else 8 - (self.height % 8)
+
+    @torch.inference_mode()
+    def pad_frame(self, frame):
+        frame = F.pad(frame, [0, self.padWidth, 0, self.padHeight])
+        return frame
+
 
     @torch.inference_mode()
     def run(self, frame: np.ndarray) -> np.ndarray:
@@ -117,7 +130,10 @@ class Upscaler:
                 frame = frame.cuda(non_blocking=True)
                 if self.half:
                     frame = frame.half()
-
+            
+            if self.padWidth != 0 or self.padHeight != 0:
+                frame = self.pad_frame(frame)
+                
             frame = self.model(frame)
             frame = frame.squeeze(0).permute(1, 2, 0).mul_(255).byte()
 
@@ -129,6 +145,152 @@ class Upscaler:
 
             return frame.cpu().numpy()
 
+
+class shuffleCuganDirectML:
+    def __init__(self):
+        model = (
+            r"G:\TheAnimeScripter\2x_AnimeJaNai_HD_V3_Compact_583k-fp16.onnx"
+        )
+
+        print(f"Using model: {model}")
+
+        providers = ort.get_available_providers()
+
+        if "DmlExecutionProvider" in providers:
+            self.model = ort.InferenceSession(
+                model, providers=["DmlExecutionProvider"]
+            )
+        else:
+            self.model = ort.InferenceSession(model, providers=["CPUExecutionProvider"])
+
+        self.IoBinding = self.model.io_binding()
+        self.frame = np.zeros((1, 3, 1080, 1920), dtype=np.float16)
+        self.output = np.zeros((1, 3, 2160, 3840), dtype=np.float16)
+
+        self.IoBinding.bind_output(
+            name='output',
+            device_type='cpu',
+            device_id=0,
+            element_type=np.float16,
+            shape=self.output.shape,  # Output shape
+            buffer_ptr=self.output.ctypes.data,
+        )
+
+    def run(self, frame: np.ndarray) -> np.ndarray:
+        frame = frame.transpose((2, 0, 1))
+        frame = frame.reshape(1, 3, 1080, 1920)
+        frame = frame / 255
+        frame = frame.astype(np.float16)
+
+        np.copyto(self.frame, frame)
+
+        self.IoBinding.bind_input(
+            name='input',
+            device_type='cpu',
+            device_id=0,
+            element_type=np.float16,
+            shape=self.frame.shape,
+            buffer_ptr=self.frame.ctypes.data,
+        )
+
+        self.model.run_with_iobinding(self.IoBinding)
+
+        output = self.output.reshape(3, 2160, 3840)
+        output = output.transpose((1, 2, 0))
+        output *= 255
+        output = np.clip(output, 0, 255)
+        output = output.astype(np.uint8)
+
+        return output
+    
+"""
+ONNXRUNTIME CUDA 
+
+NOTE:         
+    # Shufflecugan does some shape ops on the CPU which then forces clipping to [0-255]
+    # Performance is unoptimal because of it, 10fps for 1080p with ONNXRuntime CUDA whilst 16FPS with Pytorch CUDA
+    # Looking for a workaround, I assume it's pixelshuffle that's causing the issue
+    # Compact / UltraCompact / SuperUltraCompact work fine with ONNXRuntime CUDA and the performance is within margin of error (0.5FPS compared to Pytorch CUDA)
+
+class shuffleCuganCUDA:
+    def __init__(self):
+    
+
+        model = (
+            r"G:\TheAnimeScripter\2x_AnimeJaNai_HD_V3_Compact_583k-fp16.onnx"
+        )
+
+        print(f"Using model: {model}")
+
+        providers = ort.get_available_providers()
+        # Print the list of providers
+        print(providers)
+        # Specify the CUDA provider
+        if ort.get_device() == "GPU":
+            self.model = ort.InferenceSession(
+                model, providers=["CUDAExecutionProvider"]
+            )
+        else:
+            self.model = ort.InferenceSession(model, providers=["CPUExecutionProvider"])
+
+        torch.set_default_dtype(torch.float16)
+        
+        isCudaAvailable = torch.cuda.is_available()
+        self.device = torch.device("cuda" if isCudaAvailable else "cpu")
+        torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.benchmark = True
+        
+        # Binding the I/O tensors
+        # Allows for the model to be run with Torch Tensors which are faster than Numpy arrays
+        # Since frame normalizations can be done on the GPU and it allows for less data transfer between CPU and GPU
+
+        self.IoBinding = self.model.io_binding()
+
+        # Input and output tensors
+        input = torch.zeros((1, 3, 1080, 1920), device='cuda', dtype=torch.float16)
+        self.output = torch.zeros((1, 3, 2160, 3840), device='cuda', dtype=torch.float16)
+
+        # input is a placeholder tensor
+        # Pytorch overwrites the input tensor with the frame tensor
+        # The input tensor is then bound to the input of the model
+        # The output tensor is bound to the output of the model
+
+        self.IoBinding.bind_input(
+            name='input',
+            device_type='cuda',
+            device_id=0,
+            element_type=np.float16,
+            shape=tuple([1, 3, 1080, 1920]),  # Input shape
+            buffer_ptr=input.data_ptr(),
+            )
+        
+        self.IoBinding.bind_output(
+            name='output',
+            device_type='cuda',
+            device_id=0,
+            element_type=np.float16,
+            shape=tuple([1, 3, 2160, 3840]),  # Output shape
+            buffer_ptr=self.output.data_ptr(),
+        )
+
+    def run(self, frame: np.ndarray) -> np.ndarray:
+        frame = (
+            torch.from_numpy(frame)
+            .permute(2, 0, 1)
+            .unsqueeze(0)
+            .float()
+            .mul_(1 / 255)
+        )
+
+        frame = frame.contiguous()
+        frame = frame.to(self.device).half()
+
+        self.model.run_with_iobinding(self.IoBinding)
+
+        frame = self.output.squeeze(0).permute(1, 2, 0).mul_(255).byte().cpu().numpy()
+        return frame
+        
+"""
 
 """
 import os
