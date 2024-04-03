fcee902dcfb6e512715cd795788a36e1c7f62a9a
NevermindNilas <nilascontact@gmail.com>
2024-04-03 13:58:45 +0300

    we goin rainbow mode

---
+++ src/coloredPrints.py
@@ -0,0 +1,13 @@
+from colorama import Fore
+
+def green(text):
+    return Fore.GREEN + text + Fore.RESET
+    
+def red(text):
+    return Fore.RED + text + Fore.RESET
+
+def yellow(text):
+    return Fore.YELLOW + text + Fore.RESET
+
+def blue(text):
+    return Fore.BLUE + text + Fore.RESET
