8eedd2f690bba9d85380b3510519ea025ed06d31
NevermindNilas <nilascontact@gmail.com>
2024-04-26 02:30:37 +0300

    removing leftovers

--- src/rifearches/IFNet_rife416lite.py
+++ src/rifearches/IFNet_rife416lite.py
@@ -89,10 +89,6 @@ class IFBlock(nn.Module):
         )
 
     def forward(self, x, flow=None, scale=1):
-        try:
-            scale = scale.item()
-        except:
-            pass
         x = F.interpolate(
             x, scale_factor=1.0 / scale, mode="bilinear", align_corners=False
         )
