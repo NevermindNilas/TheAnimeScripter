36e1f8d61033ae984fd2aa6ed07ed4b58c62af1b
NevermindNilas <nilascontact@gmail.com>
2024-05-15 19:50:17 +0300

    fix GUI and add new options

--- src/uiLogic.py
+++ src/uiLogic.py
@@ -7,8 +7,6 @@ import logging
 from PyQt6.QtWidgets import QCheckBox, QGraphicsOpacityEffect
 from PyQt6.QtCore import QPropertyAnimation, QEasingCurve
 
-
-
 def darkUiStyleSheet() -> str:
     """
     Returns the stylesheet for the UI,
@@ -32,7 +30,7 @@ def darkUiStyleSheet() -> str:
         }
 
         QPushButton:hover {
-            background-color: #4A90E2;
+            background-color: #A0A0A0;
         }
 
         QLineEdit {
@@ -71,124 +69,65 @@ def darkUiStyleSheet() -> str:
         
     """
 
-
-def lightUiStyleSheet() -> str:
-    """
-    Returns the stylesheet for the UI,
-    These are Global preset styles meant to be used in the entire application
-    """
-    return """
-        QMainWindow {
-            background-color: #E9ECEC;
-        }
-        
-        QWidget {
-            background-color: #E9ECEC;
-            color: #000000;
-        }
-
-        QPushButton {
-            background-color: #B0BEC5;
-            color: #000000;
-            border: none;
-            border-radius: 5px;
-            padding: 5px 10px;
-        }
-
-        QPushButton:hover {
-            background-color: #78909C;
-        }
-
-        QLineEdit {
-            background-color: #CFD8DC;
-            color: #000000;
-            border: none;
-            border-radius: 5px;
-            padding: 5px;
-        }
-
-        QCheckBox {
-            color: #000000;
-            padding: 5px;
-        }
-
-        QTextEdit {
-            background-color: #CFD8DC;
-            color: #000000;
-            border: none;
-            border-radius: 5px;
-            padding: 5px;
-        }
-
-        QGroupBox {
-            border: 1px solid #B0BEC5;
-            border-radius: 5px;
-            margin-top: 1ex;
-        }
-
-        QGroupBox::title {
-            subcontrol-origin: margin;
-            subcontrol-position: top center;
-            padding: -7px 5px 0 5px;
-        }
-    """
-
-
 def runCommand(self, mainPath, settingsFile) -> None:
-    mainExePath = os.path.join(mainPath, "main.exe")
-    if not os.path.isfile(mainExePath):
-        try:
-            mainExePath = os.path.join(mainPath, "main.py")
-            command = ["python", mainExePath]
-        except FileNotFoundError:
-            self.outputWindow.append("main.exe nor main.py not found")
-            return
-    else:
-        command = [mainExePath]
+    try:
+        mainExePath = os.path.join(mainPath, "main.exe")
+        if not os.path.isfile(mainExePath):
+            try:
+                mainExePath = os.path.join(mainPath, "main.py")
+                command = ["python", mainExePath]
+            except FileNotFoundError:
+                self.outputWindow.append("main.exe nor main.py not found")
+                return
+        else:
+            command = [mainExePath]
 
-    loadSettingsFile = json.load(open(settingsFile))
+        loadSettingsFile = json.load(open(settingsFile))
 
-    for option in loadSettingsFile:
-        loweredOption = option.lower()
-        loweredOptionValue = str(loadSettingsFile[option]).lower()
+        for option in loadSettingsFile:
+            loweredOption = option.lower()
+            loweredOptionValue = str(loadSettingsFile[option]).lower()
 
-        if loweredOptionValue == "true":
-            loweredOptionValue = "1"
-        elif loweredOptionValue == "false":
-            loweredOptionValue = "0"
+            if loweredOptionValue == "true":
+                loweredOptionValue = "1"
+            elif loweredOptionValue == "false":
+                loweredOptionValue = "0"
 
-        if loweredOption == "output" and loweredOptionValue == "":
-            continue
+            if loweredOption == "output" and loweredOptionValue == "":
+                continue
 
-        command.append(f"--{loweredOption} {loweredOptionValue}")
+            command.append(f"--{loweredOption} {loweredOptionValue}")
 
-    command = " ".join(command)
-    print(command)
+        command = " ".join(command)
+        print(command)
+        def runSubprocess(command):
+            try:
+                process = subprocess.Popen(
+                    command,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    bufsize=0,
+                    universal_newlines=True,
+                )
+                while True:
+                    output = process.stdout.readline()
+                    if output == "" and process.poll() is not None:
+                        break
+                    if output:
+                        self.outputWindow.append(output.strip())
+            except Exception as e:
+                self.outputWindow.append(
+                    f"An error occurred while running the command, {e}"
+                )
+                logging.error(f"An error occurred while running, {e}")
 
-    def runSubprocess(command):
-        try:
-            process = subprocess.Popen(
-                command,
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                bufsize=0,
-                universal_newlines=True,
-            )
-            while True:
-                output = process.stdout.readline()
-                if output == "" and process.poll() is not None:
-                    break
-                if output:
-                    self.outputWindow.append(output.strip())
-        except Exception as e:
-            self.outputWindow.append(
-                f"An error occurred while running the command, {e}"
-            )
-            logging.error(f"An error occurred while running, {e}")
 
+        threading.Thread(target=runSubprocess, args=(command,)).start()
 
-    threading.Thread(target=runSubprocess, args=(command,)).start()
+    except Exception as e:
+        self.outputWindow.append(f"An error occurred while running the command, {e}")
+        logging.error(f"An error occurred while running, {e}")
 
 
 class StreamToTextEdit:
@@ -238,6 +177,7 @@ def saveSettings(self, settingsFile):
             "dedup_method": self.dedupMethodDropdown.currentText(),
             "depth_method": self.depthMethodDropdown.currentText(),
             "encode_method": self.encodeMethodDropdown.currentText(),
+            "resize_method": self.resizeMethodDropdown.currentText(),
             "audio": self.keepaudioCheckbox.isChecked(),
             "benchmark": self.benchmarkmodeCheckbox.isChecked(),
         }
@@ -272,10 +212,17 @@ def dropdownsLabels(method):
                 "RealESRGAN",
                 "Span",
                 "OmniSR",
-                "ShuffleCugan-DirectML",
+                "ApiSR",
+                "ShuffleCugan-TensorRT",
+                "Span-TensorRT",
+                "Compact-TensorRT",
+                "UltraCompact-TensorRT",
+                "SuperUltraCompact-TensorRT",
                 "Cugan-DirectML",
                 "RealESRGAN-DirectML",
                 "Span-DirectML",
+                "ShuffleCugan-NCNN",
+                "Span-NCNN",
             ]
         case "Interpolation":
             return [
@@ -283,37 +230,46 @@ def dropdownsLabels(method):
                 "Rife4.15-Lite",
                 "Rife4.15",
                 "Rife4.6",
+                "Rife4.15-TensorRT",
+                "Rife4.15-lite-TensorRT",
+                "Rife4.6-TensorRT",
+                "Rife4.16-Lite-NCNN",
+                "Rife4.15-NCNN",
+                "Rife4.15-lite-NCNN",
+                "Rife4.6-NCNN",
                 "GMFSS",
             ]
         case "Denoise":
             return ["DPIR", "SCUNet", "NAFNet", "Span"]
         case "Dedup":
-            return ["SSIM", "FFMPEG", "MSE"]
+            return ["SSIM", "FFMPEG", "MSE", "SSIM-CUDA"]
         case "Depth":
             return ["Small", "Base", "Large"]
         case "Encode":
             return {
-                "x264": ("x264", "-c:v libx264 -preset fast -crf 16"),
+                "x264": ("x264", "-c:v libx264 -preset fast -crf 17"),
                 "x264_animation": (
                     "x264_animation",
-                    "-c:v libx264 -preset fast -tune animation -crf 16",
+                    "-c:v libx264 -preset fast -tune animation -crf 17",
                 ),
-                "x265": ("x265", "-c:v libx265 -preset fast -crf 16"),
-                "nvenc_h264": ("nvenc_h264", "-c:v h264_nvenc -preset p1 -cq 16"),
-                "nvenc_h265": ("nvenc_h265", "-c:v hevc_nvenc -preset p1 -cq 16"),
+                "x265": ("x265", "-c:v libx265 -preset fast -crf 17"),
+                "nvenc_h264": ("nvenc_h264", "-c:v h264_nvenc -preset p1 -cq 17"),
+                "nvenc_h265": ("nvenc_h265", "-c:v hevc_nvenc -preset p1 -cq 17"),
                 "qsv_h264": (
                     "qsv_h264",
-                    "-c:v h264_qsv -preset veryfast -global_quality 16",
+                    "-c:v h264_qsv -preset veryfast -global_quality 17",
                 ),
                 "qsv_h265": (
                     "qsv_h265",
-                    "-c:v hevc_qsv -preset veryfast -global_quality 16",
+                    "-c:v hevc_qsv -preset veryfast -global_quality 17",
                 ),
-                "nvenc_av1": ("nvenc_av1", "-c:v av1_nvenc -preset p1 -cq 16"),
-                "av1": ("av1", "-c:v libsvtav1 -preset 8 -crf 16"),
-                "h264_amf": ("h264_amf", "-c:v h264_amf -quality speed -rc cqp -qp 16"),
-                "hevc_amf": ("hevc_amf", "-c:v hevc_amf -quality speed -rc cqp -qp 16"),
-                "vp9": ("vp9", "-c:v libvpx-vp9 -crf 16"),
+                "nvenc_av1": ("nvenc_av1", "-c:v av1_nvenc -preset p1 -cq 17"),
+                "av1": ("av1", "-c:v libsvtav1 -preset 8 -crf 17"),
+                "h264_amf": ("h264_amf", "-c:v h264_amf -quality speed -rc cqp -qp 17"),
+                "hevc_amf": ("hevc_amf", "-c:v hevc_amf -quality speed -rc cqp -qp 17"),
+                "vp9": ("vp9", "-c:v libvpx-vp9 -crf 17"),
                 "qsv_vp9": ("qsv_vp9", "-c:v vp9_qsv -preset veryfast"),
-                "prores": ("prores", "-c:v prores_ks -profile:v 4 -qscale:v 16"),
+                "prores": ("prores", "-c:v prores_ks -profile:v 4 -qscale:v 17"),
             }
+        case "Resize":
+            return ["Bilinear", "Bicubic", "Lanczos", "Nearest", "Spline", "Spline16", "Spline36"]
