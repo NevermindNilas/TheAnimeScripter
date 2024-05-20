10a8d8f33ebbfffcec7f3494260240f100051171
NevermindNilas <nilascontact@gmail.com>
2024-05-20 15:09:10 +0300

    trying hotfix

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if getattr(sys, "frozen", False):
 else:
     mainPath = os.path.dirname(os.path.abspath(__file__))
 
-scriptVersion = "1.7.1"
+scriptVersion = "1.7.2"
 warnings.filterwarnings("ignore")
 
 
