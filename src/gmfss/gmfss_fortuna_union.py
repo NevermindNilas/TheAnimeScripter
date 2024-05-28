ab779d874405305d862d687aa82481684dcda2c5
NevermindNilas <nilascontact@gmail.com>
2024-05-28 16:39:10 +0300

    fix gmfss and fix middle cropping at 4k

--- src/gmfss/gmfss_fortuna_union.py
+++ src/gmfss/gmfss_fortuna_union.py
@@ -75,7 +75,30 @@ class GMFSS:
             self.model.half()
             self.dtype = torch.half
 
-        self.I0 = None
+        self.I0 = torch.zeros(
+            1,
+            3,
+            self.height + self.padding[3],
+            self.width + self.padding[1],
+            dtype=torch.float16
+            if self.half
+            else torch.float32,
+            device=self.device,
+        )
+
+        self.I1 = torch.zeros(
+            1,
+            3,
+            self.height + self.padding[3],
+            self.width + self.padding[1],
+            dtype=torch.float16
+            if self.half
+            else torch.float32,
+            device=self.device,
+        )
+
+        self.stream = torch.cuda.Stream()
+        self.firstRun = True
 
     @torch.inference_mode() 
     def make_inference(self, n):
@@ -101,44 +124,50 @@ class GMFSS:
         return output.squeeze(0).permute(1, 2, 0).mul_(255)
 
     @torch.inference_mode()
-    def pad_image(self, img):
-        img = F.pad(img, self.padding)
-        return img
-
-    @torch.inference_mode()
-    def cacheFrame(self):
-        self.I0 = self.I1.clone()
-
-    @torch.inference_mode()
     def processFrame(self, frame):
-        frame = (
+        return (
             (
-                frame
-                .to(self.device, non_blocking=True)
+                frame.to(self.device)
                 .permute(2, 0, 1)
                 .unsqueeze(0)
                 .float()
                 if not self.half
-                else frame
-                .to(self.device, non_blocking=True)
+                else frame.to(self.device)
                 .permute(2, 0, 1)
                 .unsqueeze(0)
                 .half()
             )
-            .mul_(1 / 255)
+            .mul(1 / 255)
             .contiguous()
         )
 
-        if self.padding != (0, 0, 0, 0):
-            frame = F.pad(frame, [0, self.padding[1], 0, self.padding[3]])
-
-        return frame.contiguous(memory_format=torch.channels_last)
+    @torch.inference_mode()
+    def padFrame(self, frame):
+        return F.pad(frame, [0, self.padding[1], 0, self.padding[3]]) if self.padding != (0, 0, 0, 0) else frame
 
     @torch.inference_mode()
-    def run(self, I1):
-        if self.I0 is None:
-            self.I0 = self.processFrame(I1)
-            return False
+    def run(self, frame, interpolateFactor, writeBuffer):
+        with torch.cuda.stream(self.stream):
+            if self.firstRun is True:
+                self.I0 = self.processFrame(frame)
+                self.I0 = self.padFrame(self.I0)
+                self.firstRun = False
+                return
+            self.I1 = self.processFrame(frame)
+            self.I1 = self.padFrame(self.I1)
 
-        self.I1 = self.processFrame(I1)
-        return True
+            for i in range(interpolateFactor - 1):
+                timestep = torch.tensor(
+                    (i + 1) * 1.0 / self.interpolation_factor,
+                    dtype=self.dtype,
+                    device=self.device,
+                )
+                output = self.model(
+                        self.I0, self.I1, timestep
+                )
+                output = output[:, :, : self.height, : self.width]
+                output = output.mul(255.0).squeeze(0).permute(1, 2, 0)
+                self.stream.synchronize()
+                writeBuffer.write(output)
+
+            self.I0.copy_(self.I1, non_blocking=True)
