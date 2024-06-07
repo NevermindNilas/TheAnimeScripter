9156a4c2dc1ba0301b52adab1f50f5d105f2ed00
NevermindNilas <nilascontact@gmail.com>
2024-06-07 19:58:19 +0300

    testing new inference method

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if getattr(sys, "frozen", False):
 else:
     mainPath = os.path.dirname(os.path.abspath(__file__))
 
-scriptVersion = "1.8.2"
+scriptVersion = "1.8.3"
 warnings.filterwarnings("ignore")
 
 
