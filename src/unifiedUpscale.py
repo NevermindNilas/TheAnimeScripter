28273b150af09605db86a5222ff28caa5e12032d
NevermindNilas <nilascontact@gmail.com>
2024-07-26 18:17:45 +0300

    improve performance

--- src/unifiedUpscale.py
+++ src/unifiedUpscale.py
@@ -97,6 +97,9 @@ class UniversalPytorch:
                 device=self.device,
                 dtype=torch.float16 if self.half else torch.float32,
             )
+
+        #self.model = self.model.to(memory_format=torch.channels_last)
+        self.model = self.model.model.to(memory_format=torch.channels_last)
         
     def run(self, frame: torch.tensor) -> torch.tensor:
         with torch.cuda.stream(self.stream):
@@ -105,8 +108,8 @@ class UniversalPytorch:
                     self.skippedCounter += 1
                     return self.prevFrame
 
-            frame = frame.to(self.device, non_blocking=True, dtype=torch.float16 if self.half else torch.float32).permute(2, 0, 1).unsqueeze_(0).mul_(1 / 255)
-            output = self.model(frame).squeeze(0).permute(1, 2, 0).mul(255)
+            frame = frame.to(self.device, non_blocking=True, dtype=torch.float16 if self.half else torch.float32).permute(2, 0, 1).unsqueeze_(0).mul_(1 / 255).to(memory_format=torch.channels_last)
+            output = self.model(frame).to(memory_format=torch.channels_last).squeeze(0).mul(255).permute(1, 2, 0)
             self.stream.synchronize()
 
             if self.upscaleSkip is not None:
