11dcbfad8cb7f2e7cef87aa2aed6ba759b2e4956
NevermindNilas <nilascontact@gmail.com>
2024-07-29 17:04:58 +0300

    fallback to onnx 1.17.3

--- main.py
+++ main.py
@@ -259,15 +259,6 @@ class VideoProcessor:
 
 
 if __name__ == "__main__":
-    banner = """
-_____________            _______       _____                      ________            _____        _____             
-___  __/__  /______      ___    |_________(_)______ ________      __  ___/_______________(_)_________  /_____________
-__  /  __  __ \  _ \     __  /| |_  __ \_  /__  __ `__ \  _ \     _____ \_  ___/_  ___/_  /___  __ \  __/  _ \_  ___/
-_  /   _  / / /  __/     _  ___ |  / / /  / _  / / / / /  __/     ____/ // /__ _  /   _  / __  /_/ / /_ /  __/  /    
-/_/    /_/ /_/\___/      /_/  |_/_/ /_//_/  /_/ /_/ /_/\___/      /____/ \___/ /_/    /_/  _  .___/\__/ \___//_/     
-                                                                                           /_/                       
-"""
-    print(red(banner))
     logging.basicConfig(
         filename=os.path.join(mainPath, "log.txt"),
         filemode="w",
@@ -546,7 +537,7 @@ _  /   _  / / /  __/     _  ___ |  / / /  / _  / / / / /  __/     ____/ // /__ _
 
     args = argparser.parse_args()
     args = argumentChecker(args, mainPath, scriptVersion)
-
+    
     if os.path.isfile(args.input):
         print(green(f"Processing {args.input}"))
         if args.output is None:
