ba8f95a70de46b033469588ff1d508beb255acf1
NevermindNilas <nilascontact@gmail.com>
2024-08-03 22:24:00 +0300

    fix edge case with .webm and vp9

--- src/ffmpegSettings.py
+++ src/ffmpegSettings.py
@@ -653,28 +653,51 @@ class WriteBuffer:
             fileExtension = os.path.splitext(self.output)[1]
             mergedFile = os.path.splitext(self.output)[0] + "_merged" + fileExtension
 
-            ffmpegCommand = [
-                self.ffmpegPath,
-                "-v",
-                "error",
-                "-stats",
-                "-i",
-                self.input,
-                "-i",
-                self.output,
-                "-c:v",
-                "copy",
-                "-c:a",
-                "copy",
-                "-map",
-                "1:v:0",
-                "-map",
-                "0:a:0",
-                "-shortest",
-                "-y",
-                mergedFile,
-            ]
-    
+            if not self.output.endswith(".webm"):
+                ffmpegCommand = [
+                    self.ffmpegPath,
+                    "-v",
+                    "error",
+                    "-stats",
+                    "-i",
+                    self.input,
+                    "-i",
+                    self.output,
+                    "-c:v",
+                    "copy",
+                    "-c:a",
+                    "copy",
+                    "-map",
+                    "1:v:0",
+                    "-map",
+                    "0:a:0",
+                    "-shortest",
+                    "-y",
+                    mergedFile,
+                ]
+            else:
+                ffmpegCommand = [
+                    self.ffmpegPath,
+                    "-v",
+                    "error",
+                    "-stats",
+                    "-i",
+                    self.input,
+                    "-i",
+                    self.output,
+                    "-c:v",
+                    "copy",
+                    "-c:a",
+                    "libopus",
+                    "-map",
+                    "1:v:0",
+                    "-map",
+                    "0:a:0",
+                    "-shortest",
+                    "-y",
+                    mergedFile,
+                ]
+            
             logging.info(f"Merging audio with: {' '.join(ffmpegCommand)}")
     
             subprocess.run(ffmpegCommand)
