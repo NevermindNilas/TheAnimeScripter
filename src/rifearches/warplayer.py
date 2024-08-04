cd4613fea9b14d2694290454debbee94bec23bb6
NevermindNilas <nilascontact@gmail.com>
2024-08-04 21:43:46 +0300

    update decode

--- src/rifearches/warplayer.py
+++ src/rifearches/warplayer.py
@@ -1,5 +1,6 @@
 import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tenGrid = None
 xNorm = None
 yNorm = None
@@ -8,8 +9,8 @@ def warp(tenInput, tenFlow):
     global tenGrid, xNorm, yNorm
     if tenGrid is None or tenGrid.size() != tenFlow.size():
         N, _, H, W = tenFlow.shape
-        tenHorizontal = torch.linspace(-1.0, 1.0, W, device=tenInput.device).view(1, 1, 1, W).expand(N, -1, H, -1)
-        tenVertical = torch.linspace(-1.0, 1.0, H, device=tenInput.device).view(1, 1, H, 1).expand(N, -1, -1, W)
+        tenHorizontal = torch.linspace(-1.0, 1.0, W, device=device).view(1, 1, 1, W).expand(N, -1, H, -1)
+        tenVertical = torch.linspace(-1.0, 1.0, H, device=device).view(1, 1, H, 1).expand(N, -1, -1, W)
         tenGrid = torch.cat([tenHorizontal, tenVertical], 1)
 
         xNorm = (W - 1.0) / 2.0
