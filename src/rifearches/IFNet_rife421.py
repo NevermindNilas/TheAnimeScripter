1ac48e185f0bfc5216039f3da1d8697cd786fcb0
NevermindNilas <nilascontact@gmail.com>
2024-08-05 11:22:04 +0300

    fix issues with rife cuda and add ensemble to rife4.21

--- src/rifearches/IFNet_rife421.py
+++ src/rifearches/IFNet_rife421.py
@@ -5,21 +5,38 @@ from .warplayer import warp
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
     return nn.Sequential(
-        nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
-                  padding=padding, dilation=dilation, bias=True),        
-        nn.LeakyReLU(0.2, True)
+        nn.Conv2d(
+            in_planes,
+            out_planes,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            bias=True,
+        ),
+        nn.LeakyReLU(0.2, True),
     )
+
 
 def conv_bn(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
     return nn.Sequential(
-        nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
-                  padding=padding, dilation=dilation, bias=False),
+        nn.Conv2d(
+            in_planes,
+            out_planes,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            bias=False,
+        ),
         nn.BatchNorm2d(out_planes),
-        nn.LeakyReLU(0.2, True)
+        nn.LeakyReLU(0.2, True),
     )
-    
+
+
 class Head(nn.Module):
     def __init__(self):
         super(Head, self).__init__()
@@ -41,24 +58,25 @@ class Head(nn.Module):
             return [x0, x1, x2, x3]
         return x3
 
+
 class ResConv(nn.Module):
     def __init__(self, c, dilation=1):
         super(ResConv, self).__init__()
-        self.conv = nn.Conv2d(c, c, 3, 1, dilation, dilation=dilation, groups=1\
-)
+        self.conv = nn.Conv2d(c, c, 3, 1, dilation, dilation=dilation, groups=1)
         self.beta = nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
         self.relu = nn.LeakyReLU(0.2, True)
 
     def forward(self, x):
         return self.relu(self.conv(x) * self.beta + x)
 
+
 class IFBlock(nn.Module):
     def __init__(self, in_planes, c=64):
         super(IFBlock, self).__init__()
         self.conv0 = nn.Sequential(
-            conv(in_planes, c//2, 3, 2, 1),
-            conv(c//2, c, 3, 2, 1),
-            )
+            conv(in_planes, c // 2, 3, 2, 1),
+            conv(c // 2, c, 3, 2, 1),
+        )
         self.convblock = nn.Sequential(
             ResConv(c),
             ResConv(c),
@@ -70,36 +88,46 @@ class IFBlock(nn.Module):
             ResConv(c),
         )
         self.lastconv = nn.Sequential(
-            nn.ConvTranspose2d(c, 4*13, 4, 2, 1),
-            nn.PixelShuffle(2)
+            nn.ConvTranspose2d(c, 4 * 13, 4, 2, 1), nn.PixelShuffle(2)
         )
 
     def forward(self, x, flow=None, scale=1):
-        x = F.interpolate(x, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
+        x = F.interpolate(
+            x, scale_factor=1.0 / scale, mode="bilinear", align_corners=False
+        )
         if flow is not None:
-            flow = F.interpolate(flow, scale_factor= 1. / scale, mode="bilinear", align_corners=False) * 1. / scale
+            flow = (
+                F.interpolate(
+                    flow, scale_factor=1.0 / scale, mode="bilinear", align_corners=False
+                )
+                * 1.0
+                / scale
+            )
             x = torch.cat((x, flow), 1)
         feat = self.conv0(x)
         feat = self.convblock(feat)
         tmp = self.lastconv(feat)
-        tmp = F.interpolate(tmp, scale_factor=scale, mode="bilinear", align_corners=False)
+        tmp = F.interpolate(
+            tmp, scale_factor=scale, mode="bilinear", align_corners=False
+        )
         flow = tmp[:, :4] * scale
         mask = tmp[:, 4:5]
         feat = tmp[:, 5:]
         return flow, mask, feat
-        
+
+
 class IFNet(nn.Module):
     def __init__(self, ensemble=False, scale=1, interpolateFactor=2):
         super(IFNet, self).__init__()
-        self.block0 = IFBlock(7+16, c=256)
-        self.block1 = IFBlock(8+4+16+8, c=192)
-        self.block2 = IFBlock(8+4+16+8, c=96)
-        self.block3 = IFBlock(8+4+16+8, c=48)
+        self.block0 = IFBlock(7 + 16, c=256)
+        self.block1 = IFBlock(8 + 4 + 16 + 8, c=192)
+        self.block2 = IFBlock(8 + 4 + 16 + 8, c=96)
+        self.block3 = IFBlock(8 + 4 + 16 + 8, c=48)
         self.encode = Head()
 
         self.f0 = None
         self.f1 = None
-        self.scale_list=[8/scale, 4/scale, 2/scale, 1/scale]
+        self.scale_list = [8 / scale, 4 / scale, 2 / scale, 1 / scale]
         self.ensemble = ensemble
         self.counter = 1
         self.interpolateFactor = interpolateFactor
@@ -107,7 +135,7 @@ class IFNet(nn.Module):
 
     def cache(self):
         self.f0.copy_(self.f1, non_blocking=True)
-    
+
     def cacheReset(self, frame):
         self.f0 = self.encode(frame[:, :3])
 
@@ -135,13 +163,18 @@ class IFNet(nn.Module):
         for i in range(4):
             if flow is None:
                 flow, mask, feat = self.blocks[i](
-                    torch.cat((img0[:, :3], img1[:, :3], self.f0, self.f1, timestep), 1),
+                    torch.cat(
+                        (img0[:, :3], img1[:, :3], self.f0, self.f1, timestep), 1
+                    ),
                     None,
                     scale=self.scale_list[i],
                 )
                 if self.ensemble:
-                    f_, m_ = self.blocks[i](
-                        torch.cat((img1[:, :3], img0[:, :3], self.f1, self.f0, 1 - timestep), 1),
+                    f_, m_, feat_ = self.blocks[i](
+                        torch.cat(
+                            (img1[:, :3], img0[:, :3], self.f1, self.f0, 1 - timestep),
+                            1,
+                        ),
                         None,
                         scale=self.scale_list[i],
                     )
@@ -167,7 +200,7 @@ class IFNet(nn.Module):
                     scale=self.scale_list[i],
                 )
                 if self.ensemble:
-                    f_, m_ = self.blocks[i](
+                    f_, m_, feat_ = self.blocks[i](
                         torch.cat(
                             (
                                 warped_img1[:, :3],
@@ -176,6 +209,7 @@ class IFNet(nn.Module):
                                 wf0,
                                 1 - timestep,
                                 -mask,
+                                feat_,
                             ),
                             1,
                         ),
