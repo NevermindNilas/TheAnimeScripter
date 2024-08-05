9c3d2e0f6c6433d959f4ae1d14ffea4ef490af7a
NevermindNilas <nilascontact@gmail.com>
2024-08-05 11:22:04 +0300

    fix issues with rife cuda and add ensemble to rife4.21

--- src/rifearches/warplayer.py
+++ src/rifearches/warplayer.py
@@ -5,19 +5,30 @@ tenGrid = None
 xNorm = None
 yNorm = None
 
+
 def warp(tenInput, tenFlow):
     global tenGrid, xNorm, yNorm
     if tenGrid is None or tenGrid.size() != tenFlow.size():
         N, _, H, W = tenFlow.shape
-        tenHorizontal = torch.linspace(-1.0, 1.0, W, device=device).view(1, 1, 1, W).expand(N, -1, H, -1)
-        tenVertical = torch.linspace(-1.0, 1.0, H, device=device).view(1, 1, H, 1).expand(N, -1, -1, W)
+        tenHorizontal = (
+            torch.linspace(-1.0, 1.0, W, device=device)
+            .view(1, 1, 1, W)
+            .expand(N, -1, H, -1)
+        )
+        tenVertical = (
+            torch.linspace(-1.0, 1.0, H, device=device)
+            .view(1, 1, H, 1)
+            .expand(N, -1, -1, W)
+        )
         tenGrid = torch.cat([tenHorizontal, tenVertical], 1)
 
-        xNorm = (W - 1.0) / 2.0
-        yNorm = (H - 1.0) / 2.0
-
-    tenFlow[:, 0:1, :, :].div_(xNorm)
-    tenFlow[:, 1:2, :, :].div_(yNorm)
+    tenFlow = torch.cat(
+        [
+            tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0),
+            tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0),
+        ],
+        1,
+    )
 
     g = (tenGrid + tenFlow).permute(0, 2, 3, 1)
     return torch.nn.functional.grid_sample(
@@ -25,5 +36,5 @@ def warp(tenInput, tenFlow):
         grid=g,
         mode="bilinear",
         padding_mode="border",
-        align_corners=True
+        align_corners=True,
     )
