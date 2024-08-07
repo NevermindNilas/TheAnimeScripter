3e20d3dee5e4164f93994fa22791b7e82a30df1b
NevermindNilas <nilascontact@gmail.com>
2024-08-08 01:02:20 +0300

    upgrade to python 3.12

--- src/ytdlp.py
+++ src/ytdlp.py
@@ -4,6 +4,7 @@ import logging
 from inquirer import List, prompt
 from yt_dlp import YoutubeDL
 
+
 class VideoDownloader:
     def __init__(
         self,
@@ -26,8 +27,8 @@ class VideoDownloader:
         questions = [
             List(
                 "resolution",
-                message="Select the resolution you want to download, use up and down arrow keys to navigate and press enter to select:",
-                choices=resolutions,
+                message="Select the resolution you want to download (width x height), use up and down arrow keys to navigate and press enter to select:",
+                choices=[f"{w}x{h}" for w, h in resolutions],
             ),
         ]
 
@@ -37,9 +38,10 @@ class VideoDownloader:
             exit(1)
 
         self.resolution = answers["resolution"]
+        self.width, self.height = map(int, self.resolution.split("x"))
 
-        if self.resolution > 1080 and self.ae:
-            toPrint = f"The selected resolution {self.resolution} is higher than 1080p, this will require an aditional step of encoding the video for compatibility with After Effects"
+        if self.height > 1080 and self.ae:
+            toPrint = f"The selected resolution {self.resolution} is higher than 1080p, this will require an additional step of encoding the video for compatibility with After Effects"
             logging.warning(toPrint)
             print(toPrint)
         else:
@@ -59,11 +61,12 @@ class VideoDownloader:
             info_dict = ydl.extract_info(self.link, download=False)
             formats = info_dict.get("formats", [])
             resolutions = [
-                f.get("height")
+                (f.get("width"), f.get("height"))
                 for f in formats
-                if f.get("height") and f.get("height") >= 240
+                if f.get("width") and f.get("height") and f.get("height") >= 240
             ]
-            return sorted(set(resolutions), reverse=True)
+
+            return sorted(set(resolutions), key=lambda x: x[1], reverse=True)
 
     def downloadVideo(self):
         options = self.getOptions()
@@ -71,9 +74,9 @@ class VideoDownloader:
             ydl.download([self.link])
 
     def getOptions(self):
-        if self.resolution > 1080 and self.ae:
+        if self.height > 1080 and self.ae:
             return {
-                "format": f"bestvideo[height<={self.resolution}]+bestaudio[ext=m4a]/best[ext=mp4]",
+                "format": f"bestvideo[height<={self.height}][width<={self.width}]+bestaudio[ext=m4a]/best[ext=mp4]",
                 "outtmpl": os.path.splitext(self.output)[0],
                 "ffmpeg_location": os.path.dirname(self.ffmpegPath),
                 "quiet": True,
@@ -89,7 +92,7 @@ class VideoDownloader:
             }
         else:
             return {
-                "format": f"bestvideo[height<={self.resolution}][vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4]",
+                "format": f"bestvideo[height<={self.height}][width<={self.width}][vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4]",
                 "outtmpl": self.output,
                 "ffmpeg_location": os.path.dirname(self.ffmpegPath),
                 "quiet": True,
