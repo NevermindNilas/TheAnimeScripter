b7950c8bf3b24bf885b6b15de16d5c293391f881
NevermindNilas <nilascontact@gmail.com>
2024-08-16 21:41:03 +0300

    modify banner

--- src/coloredPrints.py
+++ src/coloredPrints.py
@@ -1,13 +1,39 @@
 from colored import fg, attr
+import random
 
 def green(text):
-    return '%s%s%s' % (fg('green'), text, attr('reset'))
+    return "%s%s%s" % (fg("green"), text, attr("reset"))
 
 def red(text):
-    return '%s%s%s' % (fg('red'), text, attr('reset'))
+    return "%s%s%s" % (fg("red"), text, attr("reset"))
 
 def yellow(text):
-    return '%s%s%s' % (fg('yellow'), text, attr('reset'))
+    return "%s%s%s" % (fg("yellow"), text, attr("reset"))
 
 def blue(text):
-    return '%s%s%s' % (fg('blue'), text, attr('reset'))
+    return "%s%s%s" % (fg("blue"), text, attr("reset"))
+
+def magenta(text):
+    return "%s%s%s" % (fg("magenta"), text, attr("reset"))
+
+def cyan(text):
+    return "%s%s%s" % (fg("cyan"), text, attr("reset"))
+
+def rainbow(text):
+    colors = ["red", "yellow", "green", "blue", "magenta", "cyan"]
+    coloredText = ""
+    for i, char in enumerate(text):
+        color = colors[i % len(colors)]
+        coloredText += "%s%s%s" % (fg(color), char, attr("reset"))
+    return coloredText
+
+def gradient(text):
+    colors = ["white", "light_red", "red"]
+    coloredText = ""
+    for i, char in enumerate(text):
+        baseIndex = int((i / len(text)) * (len(colors) - 1))
+        randomOffset = random.randint(-1, 1)
+        colorIndex = min(max(baseIndex + randomOffset, 0), len(colors) - 1)
+        color = colors[colorIndex]
+        coloredText += "%s%s%s" % (fg(color), char, attr("reset"))
+    return coloredText
