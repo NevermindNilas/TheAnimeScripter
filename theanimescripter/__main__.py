6de5aff4d27f5132bdf63e05e8105a165be006fd
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-08-29 03:42:01 +0000

    fix missing DLL problem

--- theanimescripter/__main__.py
+++ theanimescripter/__main__.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import os
+import sys
+
+os.add_dll_directory(
+    str((Path(sys.exec_prefix) / "app_packages/Library/bin").resolve())
+)
 from theanimescripter.app import main
 
 if __name__ == "__main__":
