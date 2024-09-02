e0437d3a44be62410a82ce968a44df93bc68808d
NevermindNilas <nialstheofficial@gmail.com>
2024-09-02 17:51:38 +0300

    fix linux lite issues

--- src/ffmpegSettings.py
+++ src/ffmpegSettings.py
@@ -309,8 +309,14 @@ class BuildBuffer:
                 if not rawFrame:
                     self.readBuffer.put(None)
                     break
+
+                frame = torch.from_numpy(cv2.cvtColor(np.frombuffer(rawFrame, dtype=np.uint8).reshape(reshape), cv2.COLOR_YUV2RGB_I420))
+                try:
+                    frame = frame.pin_memory()
+                except:
+                    pass
                 
-                self.readBuffer.put(torch.from_numpy(cv2.cvtColor(np.frombuffer(rawFrame, dtype=np.uint8).reshape(reshape), cv2.COLOR_YUV2RGB_I420)).pin_memory())
+                self.readBuffer.put(frame)
                 self.decodedFrames += 1
 
         except Exception as e:
