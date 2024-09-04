76eb10cb09f9c291364dda180cafe13d92d41aca
NevermindNilas <nilascontact@gmail.com>
2024-09-04 19:36:08 +0300

    udpate ffmpegSettings.py

--- src/ffmpegSettings.py
+++ src/ffmpegSettings.py
@@ -650,11 +650,10 @@ class WriteBuffer:
                             break
 
                         if self.bitDepth == "8bit":
-                            frame = frame.to(torch.uint8).cpu().numpy()
+                            frame = frame.clamp(0, 255).to(torch.uint8).cpu().numpy()
                         else:
                             frame = (
-                                frame
-                                #.clamp(0, 255)
+                                frame.clamp(0, 255)
                                 .to(torch.float32)
                                 .mul(257)
                                 .to(torch.uint16)
