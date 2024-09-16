34efe7569604f6f65d326419e85a46d01bfe4f5a
NevermindNilas <nilascontact@gmail.com>
2024-09-16 23:03:16 +0300

    make sure output is contiguos

--- src/unifiedUpscale.py
+++ src/unifiedUpscale.py
@@ -300,7 +300,7 @@ class UniversalTensorRT:
         if self.upscaleSkip is not None:
             self.prevFrame.copy_(output, non_blocking=True)
 
-        return output
+        return output.contiguous()
 
     def getSkippedCounter(self):
         return self.skippedCounter
