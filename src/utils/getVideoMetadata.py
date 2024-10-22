8c0463db5f8061e655ec7503956ca2de2a3183b1
NevermindNilas <nilascontact@gmail.com>
2024-10-22 21:18:31 +0300

    add yuv420p10le support

--- src/utils/getVideoMetadata.py
+++ src/utils/getVideoMetadata.py
@@ -11,10 +11,9 @@ def getVideoMetadata(inputPath, inPoint, outPoint):
     inputPath (str): The path to the video file.
     inPoint (float): The start time of the video clip.
     outPoint (float): The end time of the video clip.
-    audio (bool): A boolean indicating if the video has audio.
 
     Returns:
-    tuple: A tuple containing the width, height, fps, total frames to be processed, pixel format of the video, and a boolean indicating if the video has audio.
+    tuple: A tuple containing the width, height, fps, total frames to be processed, and a boolean indicating if the video has audio.
     """
     mediaInfo = MediaInfo.parse(inputPath)
     videoTrack = next(
@@ -31,18 +30,30 @@ def getVideoMetadata(inputPath, inPoint, outPoint):
     width = videoTrack.width
     height = videoTrack.height
     fps = float(videoTrack.frame_rate)
-    nframes = int(videoTrack.frame_count)
-    codec = videoTrack.codec
-    pixFmt = videoTrack.pixel_format
+    nFrames = int(videoTrack.frame_count)
+    codec = videoTrack.format
+    chromaSubsampling = videoTrack.chroma_subsampling
+    bitDepth = videoTrack.bit_depth
+    colorSpace = videoTrack.color_space
 
-    duration = round(nframes / fps, 2) if fps else 0
+    if colorSpace == "RGB":
+        pixFmt = f"rgb{bitDepth}le"
+    elif colorSpace == "YUV":
+        if bitDepth == "8":
+            pixFmt = f"yuv{chromaSubsampling.replace(':', '')}p"
+        else:
+            pixFmt = f"yuv{chromaSubsampling.replace(':', '')}p{bitDepth}le"
+    else:
+        pixFmt = "unknown"
+
+    duration = round(nFrames / fps, 2) if fps else 0
     inOutDuration = round((outPoint - inPoint) / fps, 2) if fps else 0
 
     # Calculate total frames from inPoint to outPoint
     if outPoint != 0:
         totalFramesToBeProcessed = int((outPoint - inPoint) * fps)
     else:
-        totalFramesToBeProcessed = nframes
+        totalFramesToBeProcessed = nFrames
 
     hasAudio = audioTrack is not None
 
@@ -53,13 +64,13 @@ def getVideoMetadata(inputPath, inPoint, outPoint):
     Height: {height}
     AspectRatio: {round(width/height, 2)}
     FPS: {round(fps, 2)}
-    Number of total frames: {nframes}
     Codec: {codec}
+    Pixel Format: {pixFmt}
     Duration: {duration} seconds
     In-Out Duration: {inOutDuration} seconds
+    Number of total frames: {nFrames}
     Total frames to be processed: {totalFramesToBeProcessed}
-    Pixel Format: {pixFmt}
     Has Audio: {hasAudio}""")
     )
 
-    return width, height, fps, totalFramesToBeProcessed, hasAudio
+    return width, height, fps, totalFramesToBeProcessed, hasAudio, pixFmt
