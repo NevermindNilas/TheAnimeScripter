8da78b546387fbd058bc83345e034ea8b0471984
NevermindNilas <nilascontact@gmail.com>
2024-10-29 21:37:08 +0200

    add progress bar to autoclip

--- src/autoclip/autoclip.py
+++ src/autoclip/autoclip.py
@@ -21,10 +21,8 @@ class AutoClip:
             end_time = FrameTimecode(self.outPoint, video_manager.get_framerate())
             video_manager.set_duration(start_time=start_time, end_time=end_time)
 
-        # video_manager.set_downscale_factor()
-
         video_manager.start()
-        scene_manager.detect_scenes(frame_source=video_manager)
+        scene_manager.detect_scenes(frame_source=video_manager, show_progress=True)
 
         scene_list = scene_manager.get_scene_list()
 
