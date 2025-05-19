12166a93d32253d856e0215c3c7676de6cd28bf3
NevermindNilas <nilascontact@gmail.com>
2025-05-19 21:41:14 +0300

    ensure torch cuda synch in rife cuda and revert to old warplayer

--- src/rifearches/warplayer.py
+++ src/rifearches/warplayer.py
@@ -1,24 +1,25 @@
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-tenGrid = None
+tenGrid = {}
 
 
 def warp(tenInput, tenFlow):
-    global tenGrid
-    if tenGrid is None or tenGrid.size() != tenFlow.size():
-        N, _, H, W = tenFlow.shape
+    k = (str(tenFlow.device), str(tenFlow.size()))
+    if k not in tenGrid:
         tenHorizontal = (
-            torch.linspace(-1.0, 1.0, W, device=device)
-            .view(1, 1, 1, W)
-            .expand(N, -1, H, -1)
+            torch.linspace(-1.0, 1.0, tenFlow.shape[3], device=device)
+            .view(1, 1, 1, tenFlow.shape[3])
+            .expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
         )
         tenVertical = (
-            torch.linspace(-1.0, 1.0, H, device=device)
-            .view(1, 1, H, 1)
-            .expand(N, -1, -1, W)
+            torch.linspace(-1.0, 1.0, tenFlow.shape[2], device=device)
+            .view(1, 1, tenFlow.shape[2], 1)
+            .expand(tenFlow.shape[0], -1, -1, tenFlow.shape[3])
         )
-        tenGrid = torch.cat([tenHorizontal, tenVertical], 1).to(tenFlow.dtype)
+        tenGrid[k] = (
+            torch.cat([tenHorizontal, tenVertical], 1).to(device).to(tenFlow.dtype)
+        )
 
     tenFlow = torch.cat(
         [
@@ -28,7 +29,7 @@ def warp(tenInput, tenFlow):
         1,
     )
 
-    g = (tenGrid + tenFlow).permute(0, 2, 3, 1)
+    g = (tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
     return torch.nn.functional.grid_sample(
         input=tenInput,
         grid=g,
